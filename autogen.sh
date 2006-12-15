13b637f1ce5dc5dbfef1998a7200854752e5ab72
ejcoumans <ejcoumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2006-12-15 18:24:31 +0000

    updated build configuration to include make, next to jam. So ./configure will create Makefile AND Jamefile.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+set -x
 
 if test ! -f configure.ac ; then
   echo "*** Please invoke this script from directory containing configure.ac."
@@ -9,17 +10,27 @@ MACROFILE=aclocal.m4
 MACRODIR=mk/autoconf
 
 rm -f $MACROFILE
+
+echo "Running aclocal"
+aclocal
+
 for i in $MACRODIR/*.m4 ; do
   cat $i >> $MACROFILE
 done
 
+
+echo "Running automake --add-missing"
+automake --add-missing
+
+echo "Running autoheader"
 autoheader
 rc=$?
 
 if test $rc -eq 0; then
+  echo "Running autoconf"
   autoconf
   rc=$?
 fi
 
-rm -f $MACROFILE
+#rm -f $MACROFILE
 exit $rc
