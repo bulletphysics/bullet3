30b1887f40690d64c4df702d8a02a2c298aab409
ejcoumans <ejcoumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2007-09-08 03:10:30 +0000

    added configuration files for collision detection. this allows to use different collision algorithms by default, and avoid linkage issues.

---
+++ src/BulletCollision/CollisionDispatch/btDefaultCollisionConfiguration.h
@@ -0,0 +1,72 @@
+/*
+Bullet Continuous Collision Detection and Physics Library
+Copyright (c) 2003-2006 Erwin Coumans  http://continuousphysics.com/Bullet/
+
+This software is provided 'as-is', without any express or implied warranty.
+In no event will the authors be held liable for any damages arising from the use of this software.
+Permission is granted to anyone to use this software for any purpose, 
+including commercial applications, and to alter it and redistribute it freely, 
+subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef BT_DEFAULT_COLLISION_CONFIGURATION
+#define BT_DEFAULT_COLLISION_CONFIGURATION
+
+#include "btCollisionConfiguration.h"
+
+///btCollisionConfiguration allows to configure Bullet collision detection
+///stack allocator size, default collision algorithms and persistent manifold pool size
+///todo: describe the meaning
+class	btDefaultCollisionConfiguration
+{
+
+	int	m_persistentManifoldPoolSize;
+	
+	int	m_stackAllocatorSize;
+
+	int collisionAlgorithmPoolSize;
+
+	int collisionAlgorithmMaxElementSize;
+
+	//default CreationFunctions, filling the m_doubleDispatch table
+	btCollisionAlgorithmCreateFunc*	m_convexConvexCreateFunc;
+	btCollisionAlgorithmCreateFunc*	m_convexConcaveCreateFunc;
+	btCollisionAlgorithmCreateFunc*	m_swappedConvexConcaveCreateFunc;
+	btCollisionAlgorithmCreateFunc*	m_compoundCreateFunc;
+	btCollisionAlgorithmCreateFunc*	m_swappedCompoundCreateFunc;
+	btCollisionAlgorithmCreateFunc*   m_emptyCreateFunc;
+
+public:
+
+	btDefaultCollisionConfiguration();
+
+	virtual ~btDefaultCollisionConfiguration();
+
+	///pool size for the persistent contact manifold
+	virtual int	getPersistentManifoldPoolSize();
+
+	virtual int	getStackAllocatorSize();
+
+	virtual int	getCollisionAlgorithmPoolSize();
+
+	virtual int	getCollisionAlgorithmMaxElementSize();
+
+	virtual btCollisionAlgorithmCreateFunc* getConvexConvexCollisionCreateFunc();
+
+	virtual btCollisionAlgorithmCreateFunc* getConvexConcaveCollisionCreateFunc();
+
+	virtual btCollisionAlgorithmCreateFunc* getSwappedConvexConcaveCollisionCreateFunc();
+
+	virtual btCollisionAlgorithmCreateFunc* getCompoundCollisionCreateFunc();
+
+	virtual btCollisionAlgorithmCreateFunc* getSwappedCompoundCollisionCreateFunc();
+
+	virtual btCollisionAlgorithmCreateFunc* getEmptyCollisionCreateFunc();
+
+};
+
+#endif //BT_DEFAULT_COLLISION_CONFIGURATION
