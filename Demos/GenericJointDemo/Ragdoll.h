7f5823ee16912bf045748efdc884e822823c0409
ejcoumans <ejcoumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2007-09-13 07:26:54 +0000

    updated ragdoll demo/Jamfile

--- Demos/GenericJointDemo/Ragdoll.h
+++ Demos/GenericJointDemo/Ragdoll.h
@@ -6,9 +6,6 @@
 #include "btBulletDynamicsCommon.h"
 
 
-#define M_PI       3.14159265358979323846
-#define M_PI_2     1.57079632679489661923
-#define M_PI_4     0.785398163397448309616
 
 
 class RagDoll
