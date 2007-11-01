2aec2a996910872e27915d4a99d628ce8ab0f815
ejcoumans <ejcoumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2007-11-01 23:42:17 +0000

    Added some comments about the btPersistentManifold (contact point cache for potential overlapping pairs) http://www.bulletphysics.com/Bullet/phpBB3/viewtopic.php?f=9&t=1608

--- src/BulletCollision/NarrowPhaseCollision/btPersistentManifold.h
+++ src/BulletCollision/NarrowPhaseCollision/btPersistentManifold.h
@@ -35,8 +35,13 @@ extern ContactDestroyedCallback	gContactDestroyedCallback;
 
 #define MANIFOLD_CACHE_SIZE 4
 
-///btPersistentManifold maintains contact points, and reduces them to 4.
-///It does contact filtering/contact reduction.
+///btPersistentManifold is a contact point cache, it stays persistent as long as objects are overlapping in the broadphase.
+///Those contact points are created by the collision narrow phase.
+///The cache can be empty, or hold 1,2,3 or 4 points. Some collision algorithms (GJK) might only add one point at a time.
+///updates/refreshes old contact points, and throw them away if necessary (distance becomes too large)
+///reduces the cache to 4 points, when more then 4 points are added, using following rules:
+///the contact point with deepest penetration is always kept, and it tries to maximuze the area covered by the points
+///note that some pairs of objects might have more then one contact manifold.
 ATTRIBUTE_ALIGNED16( class) btPersistentManifold 
 {
 
