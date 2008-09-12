50daff0848bd768620432c7056ba725b04cb56b7
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2008-09-12 21:22:30 +0000

    added install.bat used for the Windows release, and updated docs to add instruction videos and link to the forums for support.

---
+++ Extras/MayaPlugin/install.bat
@@ -0,0 +1,4 @@
+xcopy /S /Y /C /I /h bin\*.* "C:\Program Files\Autodesk\Maya2008\bin"
+xcopy /S /Y /C /I  /h icons\*.* "C:\Program Files\Autodesk\Maya2008\icons"
+xcopy /S /Y /C /I  /h mvl\*.* "C:\Program Files\Autodesk\Maya2008\mvl"
+xcopy /S /Y /C /I  /h scripts\*.* "C:\Program Files\Autodesk\Maya2008\scripts"
