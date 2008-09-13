3d2ce530d4d7ea737c6cd88f9800a594599a9b78
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2008-09-13 06:46:53 +0000

    applied patch to make child shape of a compound shape available during ray testing. Thanks to Benoit Bolsee for the patch: http://code.google.com/p/bullet/issues/detail?id=91

--- src/BulletCollision/CollisionDispatch/btCollisionWorld.cpp
+++ src/BulletCollision/CollisionDispatch/btCollisionWorld.cpp
@@ -384,12 +384,16 @@ void	btCollisionWorld::rayTestSingle(const btTransform& rayFromTrans,const btTra
 					btTransform childTrans = compoundShape->getChildTransform(i);
 					const btCollisionShape* childCollisionShape = compoundShape->getChildShape(i);
 					btTransform childWorldTrans = colObjWorldTransform * childTrans;
+					// replace collision shape so that callback can determine the triangle
+					btCollisionShape* saveCollisionShape = collisionObject->getCollisionShape();
+					collisionObject->internalSetTemporaryCollisionShape((btCollisionShape*)childCollisionShape);
 					rayTestSingle(rayFromTrans,rayToTrans,
 						collisionObject,
 						childCollisionShape,
 						childWorldTrans,
 						resultCallback);
-
+					// restore
+					collisionObject->internalSetTemporaryCollisionShape(saveCollisionShape);
 				}
 			}
 		}
@@ -576,11 +580,16 @@ void	btCollisionWorld::objectQuerySingle(const btConvexShape* castShape,const bt
 					btTransform childTrans = compoundShape->getChildTransform(i);
 					const btCollisionShape* childCollisionShape = compoundShape->getChildShape(i);
 					btTransform childWorldTrans = colObjWorldTransform * childTrans;
+					// replace collision shape so that callback can determine the triangle
+					btCollisionShape* saveCollisionShape = collisionObject->getCollisionShape();
+					collisionObject->internalSetTemporaryCollisionShape((btCollisionShape*)childCollisionShape);
 					objectQuerySingle(castShape, convexFromTrans,convexToTrans,
 						collisionObject,
 						childCollisionShape,
 						childWorldTrans,
 						resultCallback, allowedPenetration);
+					// restore
+					collisionObject->internalSetTemporaryCollisionShape(saveCollisionShape);
 				}
 			}
 		}
