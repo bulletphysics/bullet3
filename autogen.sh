e43145062047e3ab5ea1b6e3077dfe19cf5e9844
john.mccutchan <john.mccutchan@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2008-10-02 22:23:01 +0000

    Initial support of Autoconf / Automake build system
    Now running ./autogen.sh and ./configure will produce both Makefiles built from Autoconf and Automake 
    as well as Jam build files.

--- autogen.sh
+++ autogen.sh
@@ -1,25 +1,44 @@
 #! /bin/sh
 
+if [ "$USER" = "root" ]; then
+	echo "*** You cannot do this as "$USER" please use a normal user account."
+	exit 1
+fi
 if test ! -f configure.ac ; then
-  echo "*** Please invoke this script from directory containing configure.ac."
-  exit 1
+	echo "*** Please invoke this script from directory containing configure.ac."
+	exit 1
 fi
 
-MACROFILE=aclocal.m4
-MACRODIR=mk/autoconf
+#MACROFILE=aclocal.m4
+#MACRODIR=mk/autoconf
+#rm -f $MACROFILE
 
-rm -f $MACROFILE
-for i in $MACRODIR/*.m4 ; do
-  cat $i >> $MACROFILE
-done
-
-autoheader
+aclocal -I mk/autoconf
 rc=$?
 
+#for i in $MACRODIR/*.m4 ; do
+#  cat $i >> $MACROFILE
+#done
+
 if test $rc -eq 0; then
-  autoconf
-  rc=$?
+	automake --add-missing
+	rc=$?
 fi
 
-rm -f $MACROFILE
+if test $rc -eq 0; then
+	libtoolize --force
+	rc=$?
+fi
+
+
+if test $rc -eq 0; then
+	autoheader
+	rc=$?
+fi
+
+if test $rc -eq 0; then
+	autoconf
+	rc=$?
+fi
+
 exit $rc
