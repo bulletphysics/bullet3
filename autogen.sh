cb8ecc0d2fa6add3bec8c153f7f708d1f0b61030
john.mccutchan <john.mccutchan@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2008-10-03 21:00:57 +0000

    Fixes for UNIX build system

--- autogen.sh
+++ autogen.sh
@@ -9,27 +9,44 @@ if test ! -f configure.ac ; then
 	exit 1
 fi
 
-aclocal -I mk/autoconf
+echo "running aclocal"
+aclocal
 rc=$?
 
 if test $rc -eq 0; then
+	echo "running libtool"
 	libtoolize --force --automake --copy
 	rc=$?
+else
+	echo "An error occured, autogen.sh stopping."
+	exit $rc
 fi
 
 if test $rc -eq 0; then
+	echo "running automake"
 	automake --add-missing --copy
 	rc=$?
+else
+	echo "An error occured, autogen.sh stopping."
+	exit $rc
 fi
 
 if test $rc -eq 0; then
+	echo "running autoheader"
 	autoheader
 	rc=$?
+else
+	echo "An error occured, autogen.sh stopping."
+	exit $rc
 fi
 
 if test $rc -eq 0; then
+	echo "running autoconf"
 	autoconf
 	rc=$?
+else
+	echo "An error occured, autogen.sh stopping."
+	exit $rc
 fi
 
 echo "autogen.sh complete"
