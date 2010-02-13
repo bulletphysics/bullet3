510be411e65ff1b2fd2d3f86aef53b620a5e6485
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2010-02-13 22:06:33 +0000

    added missing virtual destructor

--- src/BulletCollision/Gimpact/gim_box_set.h
+++ src/BulletCollision/Gimpact/gim_box_set.h
@@ -91,6 +91,7 @@ class GIM_PRIMITIVE_MANAGER_PROTOTYPE
 {
 public:
 
+	virtual ~GIM_PRIMITIVE_MANAGER_PROTOTYPE() {}
 	//! determines if this manager consist on only triangles, which special case will be optimized
 	virtual bool is_trimesh() = 0;
 	virtual GUINT get_primitive_count() = 0;
@@ -669,3 +670,5 @@ public:
 
 
 #endif // GIM_BOXPRUNING_H_INCLUDED
+
+
