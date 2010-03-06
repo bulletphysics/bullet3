da3c174b0ef483b5a889fab2a23bc276de69256a
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2010-03-06 18:31:59 +0000

    Maya Dynamica plugin is moved to http://dynamica.googlecode.com

--- Extras/MayaPlugin/install.bat
+++
@@ -1,3 +0,0 @@
-xcopy /S /Y /C /I  /h icons\*.xpm "C:\Program Files\Autodesk\Maya8.5\icons"
-xcopy /S /Y /C /I  /h scripts\*.mel "C:\Program Files\Autodesk\Maya8.5\scripts"
-xcopy /S /Y /C /I  /h BulletMayaPluginDebug.mll "C:\Program Files\Autodesk\Maya8.5\bin\plug-ins"
