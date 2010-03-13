2f2d33d578755963582784c838acb7c38e96e9ce
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2010-03-13 20:47:21 +0000

    bugfix related to previous commit (btAssert at the wrong place) fixes for automake build system (remove references to ColladaDemo)

--- src/BulletCollision/CollisionDispatch/btCompoundCollisionAlgorithm.cpp
+++ src/BulletCollision/CollisionDispatch/btCompoundCollisionAlgorithm.cpp
@@ -115,9 +115,8 @@ public:
 	void	ProcessChildShape(btCollisionShape* childShape,int index)
 	{
 		btAssert(index>=0);
-		btAssert(index<compoundShape->getNumChildShapes());
-
 		btCompoundShape* compoundShape = static_cast<btCompoundShape*>(m_compoundColObj->getCollisionShape());
+		btAssert(index<compoundShape->getNumChildShapes());
 
 
 		//backup
