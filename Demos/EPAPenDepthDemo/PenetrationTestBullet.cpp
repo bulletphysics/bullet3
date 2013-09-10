d50be40f8e5c145c46aaa179d3efeeed6338b18b
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2013-09-10 20:15:52 +0000

    Fix Issue 2644, compile error due to use of removed stack alloc

--- Demos/EPAPenDepthDemo/PenetrationTestBullet.cpp
+++ Demos/EPAPenDepthDemo/PenetrationTestBullet.cpp
@@ -33,7 +33,7 @@
 #include "BulletCollision/NarrowPhaseCollision/btMinkowskiPenetrationDepthSolver.h"
 #include "BulletCollision/NarrowPhaseCollision/btGjkPairDetector.h"
 #include "BulletCollision/NarrowPhaseCollision/btGjkEpaPenetrationDepthSolver.h"
-#include "LinearMath/btStackAlloc.h"
+
 
 //We can use the Bullet EPA or sampling penetration depth solver, but comparison might be useful
 //#define COMPARE_WITH_SOLID35_AND_OTHER_EPA 1
@@ -299,8 +299,7 @@ static float gDepth;
 		}
 	};
 
-//2 Mb by default, could be made smaller
-btStackAlloc gStackAlloc(1024*1024*2);
+
 
 static bool TestEPA(const MyConvex& hull0, const MyConvex& hull1)
 {
@@ -346,7 +345,7 @@ static bool TestEPA(const MyConvex& hull0, const MyConvex& hull1)
 	btDiscreteCollisionDetectorInterface::ClosestPointInput input;
 	input.m_transformA = hull0.mTransform;
 	input.m_transformB = hull1.mTransform;
-	input.m_stackAlloc = &gStackAlloc;
+	
 
 	MyResult output;
 	GJK.getClosestPoints(input, output, 0);
