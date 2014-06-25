b063cfc5099484df118edbfd9d2e92d71530ae05
Erwin Coumans <erwin.coumans@gmail.com>
2014-06-24 20:24:31 -0700

    more workaround for glDrawBuffers issue

--- btgui/OpenGLWindow/GLRenderToTexture.cpp
+++ btgui/OpenGLWindow/GLRenderToTexture.cpp
@@ -66,9 +66,9 @@ bool GLRenderToTexture::enable()
 		}
 		case RENDERTEXTURE_DEPTH:
 		{
-			GLenum drawBuffers[2] = {GL_DEPTH_ATTACHMENT,0};
-                        glDrawBuffers(1, drawBuffers);
-			//glDrawBuffer(GL_NONE);
+			GLenum drawBuffers[2] = { GL_NONE, 0 };// GL_DEPTH_ATTACHMENT, 0};
+            glDrawBuffers(1, drawBuffers);
+//			glDrawBuffer(GL_NONE);
 			break;
 		}
 		default:
