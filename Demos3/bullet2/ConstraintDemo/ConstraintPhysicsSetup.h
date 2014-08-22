2c19a276943107ae9e06c9c79ce231281f0645b0
Erwin Coumans <erwin.coumans@gmail.com>
2014-08-22 14:50:42 -0700

    add missing ConstraintPhysicsSetup.* files

---
+++ Demos3/bullet2/ConstraintDemo/ConstraintPhysicsSetup.h
@@ -0,0 +1,16 @@
+#ifndef CONSTAINT_PHYSICS_SETUP_H
+#define CONSTAINT_PHYSICS_SETUP_H
+
+#include "../../../Demos/CommonRigidBodySetup.h"
+
+struct ConstraintPhysicsSetup : public CommonRigidBodySetup
+{
+	ConstraintPhysicsSetup();
+	virtual ~ConstraintPhysicsSetup();
+	virtual void initPhysics(GraphicsPhysicsBridge& gfxBridge);
+
+	virtual void stepSimulation(float deltaTime);
+
+};
+
+#endif //CONSTAINT_PHYSICS_SETUP_H
