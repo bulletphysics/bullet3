6a0ad6da3939b9728ff99357ec955ebea0f625ea
Lunkhound <lunkhound@gmail.com>
2014-12-08 13:38:28 -0800

    simulation island manager refactor for multithreading In order to simulate multiple islands at once, all the islands need to be defined up front, and cannot sequentially reuse the arrays of bodies, manifolds or constraints.  - A new Island structure is introduced inside the btSimulationIslandManager class to hold the various bodies, manifolds and constraints for each island.  - The functionallity for merging islands that existed in the InplaceSolverIslandCallback (used by btDiscreteDynamicsWorld) has been moved into the btSimulationIslandManager class (which also allowed the removal of duplicate code from the multi-body dynamics world).  - By assembling all of the Islands up front, it was possible to eliminate the sorting of manifolds and constraints by island-id.  These sorts are simply replaced with a single walk over the unsorted objects, distributing them to the appropriate island.  - Note that the btSimulationIslandManager files really do not belong under BulletCollision -- the class is only referenced by the discrete and multi-body dynamics worlds.  And now that the island manager makes use of btTypedConstraint objects (defined in BulletDynamics), it makes even less sense.

--- src/BulletCollision/CollisionDispatch/btSimulationIslandManager.h
+++ src/BulletCollision/CollisionDispatch/btSimulationIslandManager.h
@@ -25,17 +25,38 @@ class btCollisionObject;
 class btCollisionWorld;
 class btDispatcher;
 class btPersistentManifold;
+class btTypedConstraint;
 
 
 ///SimulationIslandManager creates and handles simulation islands, using btUnionFind
 class btSimulationIslandManager
 {
-	btUnionFind m_unionFind;
+public:
+    struct Island
+    {
+        // a simulation island consisting of bodies, manifolds and constraints,
+        // to be passed into a constraint solver.
+        btAlignedObjectArray<btCollisionObject*> bodyArray;
+        btAlignedObjectArray<btPersistentManifold*> manifoldArray;
+        btAlignedObjectArray<btTypedConstraint*> constraintArray;
+        int id;  // island id
+        bool isSleeping;
 
-	btAlignedObjectArray<btPersistentManifold*>  m_islandmanifold;
-	btAlignedObjectArray<btCollisionObject* >  m_islandBodies;
-	
+        void append( const Island& other );  // add bodies, manifolds, constraints to my own
+    };
+
+private:
+    btUnionFind m_unionFind;
+    btAlignedObjectArray<Island*> m_allocatedIslands;  // owner of all Islands
+    btAlignedObjectArray<Island*> m_usedIslands;  // islands actively in use
+    btAlignedObjectArray<Island*> m_freeIslands;  // islands ready to be reused
+    btAlignedObjectArray<Island*> m_lookupIslandFromId;  // big lookup table to map islandId to Island pointer
+    int m_minimumSolverBatchSize;
 	bool m_splitIslands;
+
+    Island* getIsland( int id );
+    Island* allocateIsland( int id, int numBodies );
+    void initIslandPools();
 	
 public:
 	btSimulationIslandManager();
@@ -53,16 +74,21 @@ public:
 
 	void	findUnions(btDispatcher* dispatcher,btCollisionWorld* colWorld);
 
-	
-
 	struct	IslandCallback
 	{
 		virtual ~IslandCallback() {};
 
-		virtual	void	processIsland(btCollisionObject** bodies,int numBodies,class btPersistentManifold**	manifolds,int numManifolds, int islandId) = 0;
+        virtual	void processIsland( btCollisionObject** bodies,
+                                    int numBodies,
+                                    btPersistentManifold** manifolds,
+                                    int numManifolds,
+                                    btTypedConstraint** constraints,
+                                    int numConstraints,
+                                    int islandId
+                                    ) = 0;
 	};
 
-	virtual void buildAndProcessIslands(btDispatcher* dispatcher,btCollisionWorld* collisionWorld, IslandCallback* callback);
+    virtual void buildAndProcessIslands( btDispatcher* dispatcher, btCollisionWorld* collisionWorld, btAlignedObjectArray<btTypedConstraint*>& constraints, IslandCallback* callback );
 
 	virtual void buildIslands(btDispatcher* dispatcher,btCollisionWorld* colWorld);
 
@@ -74,7 +100,14 @@ public:
 	{
 		m_splitIslands = doSplitIslands;
 	}
-
+    int getMinimumSolverBatchSize() const
+    {
+        return m_minimumSolverBatchSize;
+    }
+    void setMinimumSolverBatchSize( int sz )
+    {
+        m_minimumSolverBatchSize = sz;
+    }
 };
 
 #endif //BT_SIMULATION_ISLAND_MANAGER_H
--- src/BulletDynamics/Dynamics/btDiscreteDynamicsWorld.cpp
+++ src/BulletDynamics/Dynamics/btDiscreteDynamicsWorld.cpp
@@ -55,44 +55,12 @@ int startHit=2;
 int firstHit=startHit;
 #endif
 
-SIMD_FORCE_INLINE	int	btGetConstraintIslandId(const btTypedConstraint* lhs)
-{
-	int islandId;
-
-	const btCollisionObject& rcolObj0 = lhs->getRigidBodyA();
-	const btCollisionObject& rcolObj1 = lhs->getRigidBodyB();
-	islandId= rcolObj0.getIslandTag()>=0?rcolObj0.getIslandTag():rcolObj1.getIslandTag();
-	return islandId;
-
-}
-
-
-class btSortConstraintOnIslandPredicate
-{
-	public:
-
-		bool operator() ( const btTypedConstraint* lhs, const btTypedConstraint* rhs ) const
-		{
-			int rIslandId0,lIslandId0;
-			rIslandId0 = btGetConstraintIslandId(rhs);
-			lIslandId0 = btGetConstraintIslandId(lhs);
-			return lIslandId0 < rIslandId0;
-		}
-};
-
 struct InplaceSolverIslandCallback : public btSimulationIslandManager::IslandCallback
 {
 	btContactSolverInfo*	m_solverInfo;
 	btConstraintSolver*		m_solver;
-	btTypedConstraint**		m_sortedConstraints;
-	int						m_numConstraints;
 	btIDebugDraw*			m_debugDrawer;
 	btDispatcher*			m_dispatcher;
-
-	btAlignedObjectArray<btCollisionObject*> m_bodies;
-	btAlignedObjectArray<btPersistentManifold*> m_manifolds;
-	btAlignedObjectArray<btTypedConstraint*> m_constraints;
-
 
 	InplaceSolverIslandCallback(
 		btConstraintSolver*	solver,
@@ -100,8 +68,6 @@ struct InplaceSolverIslandCallback : public btSimulationIslandManager::IslandCal
 		btDispatcher* dispatcher)
 		:m_solverInfo(NULL),
 		m_solver(solver),
-		m_sortedConstraints(NULL),
-		m_numConstraints(0),
 		m_debugDrawer(NULL),
 		m_dispatcher(dispatcher)
 	{
@@ -115,85 +81,35 @@ struct InplaceSolverIslandCallback : public btSimulationIslandManager::IslandCal
 		return *this;
 	}
 
-	SIMD_FORCE_INLINE void setup ( btContactSolverInfo* solverInfo, btTypedConstraint** sortedConstraints,	int	numConstraints,	btIDebugDraw* debugDrawer)
+	SIMD_FORCE_INLINE void setup ( btContactSolverInfo* solverInfo, btIDebugDraw* debugDrawer)
 	{
 		btAssert(solverInfo);
 		m_solverInfo = solverInfo;
-		m_sortedConstraints = sortedConstraints;
-		m_numConstraints = numConstraints;
 		m_debugDrawer = debugDrawer;
-		m_bodies.resize (0);
-		m_manifolds.resize (0);
-		m_constraints.resize (0);
 	}
 
 
-	virtual	void	processIsland(btCollisionObject** bodies,int numBodies,btPersistentManifold**	manifolds,int numManifolds, int islandId)
+	virtual	void	processIsland( btCollisionObject** bodies,
+                                   int numBodies,
+                                   btPersistentManifold** manifolds,
+                                   int numManifolds,
+                                   btTypedConstraint** constraints,
+                                   int numConstraints,
+                                   int islandId
+                                   )
 	{
-		if (islandId<0)
-		{
-			///we don't split islands, so all constraints/contact manifolds/bodies are passed into the solver regardless the island id
-			m_solver->solveGroup( bodies,numBodies,manifolds, numManifolds,&m_sortedConstraints[0],m_numConstraints,*m_solverInfo,m_debugDrawer,m_dispatcher);
-		} else
-		{
-				//also add all non-contact constraints/joints for this island
-			btTypedConstraint** startConstraint = 0;
-			int numCurConstraints = 0;
-			int i;
-
-			//find the first constraint for this island
-			for (i=0;i<m_numConstraints;i++)
-			{
-				if (btGetConstraintIslandId(m_sortedConstraints[i]) == islandId)
-				{
-					startConstraint = &m_sortedConstraints[i];
-					break;
-				}
-			}
-			//count the number of constraints in this island
-			for (;i<m_numConstraints;i++)
-			{
-				if (btGetConstraintIslandId(m_sortedConstraints[i]) == islandId)
-				{
-					numCurConstraints++;
-				}
-			}
-
-			if (m_solverInfo->m_minimumSolverBatchSize<=1)
-			{
-				m_solver->solveGroup( bodies,numBodies,manifolds, numManifolds,startConstraint,numCurConstraints,*m_solverInfo,m_debugDrawer,m_dispatcher);
-			} else
-			{
-
-				for (i=0;i<numBodies;i++)
-					m_bodies.push_back(bodies[i]);
-				for (i=0;i<numManifolds;i++)
-					m_manifolds.push_back(manifolds[i]);
-				for (i=0;i<numCurConstraints;i++)
-					m_constraints.push_back(startConstraint[i]);
-				if ((m_constraints.size()+m_manifolds.size())>m_solverInfo->m_minimumSolverBatchSize)
-				{
-					processConstraints();
-				} else
-				{
-					//printf("deferred\n");
-				}
-			}
-		}
-	}
-	void	processConstraints()
-	{
-
-		btCollisionObject** bodies = m_bodies.size()? &m_bodies[0]:0;
-		btPersistentManifold** manifold = m_manifolds.size()?&m_manifolds[0]:0;
-		btTypedConstraint** constraints = m_constraints.size()?&m_constraints[0]:0;
-
-		m_solver->solveGroup( bodies,m_bodies.size(),manifold, m_manifolds.size(),constraints, m_constraints.size() ,*m_solverInfo,m_debugDrawer,m_dispatcher);
-		m_bodies.resize(0);
-		m_manifolds.resize(0);
-		m_constraints.resize(0);
-
-	}
+        ///we don't split islands, so all constraints/contact manifolds/bodies are passed into the solver regardless the island id
+        m_solver->solveGroup( bodies,
+                              numBodies,
+                              manifolds,
+                              numManifolds,
+                              constraints,
+                              numConstraints,
+                              *m_solverInfo,
+                              m_debugDrawer,
+                              m_dispatcher
+                              );
+    }
 
 };
 
@@ -226,6 +142,7 @@ m_latencyMotionStateInterpolation(true)
 	{
 		void* mem = btAlignedAlloc(sizeof(btSimulationIslandManager),16);
 		m_islandManager = new (mem) btSimulationIslandManager();
+        m_islandManager->setMinimumSolverBatchSize( m_solverInfo.m_minimumSolverBatchSize );
 	}
 
 	m_ownsIslandManager = true;
@@ -711,28 +628,14 @@ void	btDiscreteDynamicsWorld::solveConstraints(btContactSolverInfo& solverInfo)
 {
 	BT_PROFILE("solveConstraints");
 
-	m_sortedConstraints.resize( m_constraints.size());
-	int i;
-	for (i=0;i<getNumConstraints();i++)
-	{
-		m_sortedConstraints[i] = m_constraints[i];
-	}
 
-//	btAssert(0);
-
-
-
-	m_sortedConstraints.quickSort(btSortConstraintOnIslandPredicate());
-
-	btTypedConstraint** constraintsPtr = getNumConstraints() ? &m_sortedConstraints[0] : 0;
-
-	m_solverIslandCallback->setup(&solverInfo,constraintsPtr,m_sortedConstraints.size(),getDebugDrawer());
+	m_solverIslandCallback->setup(&solverInfo, getDebugDrawer());
 	m_constraintSolver->prepareSolve(getCollisionWorld()->getNumCollisionObjects(), getCollisionWorld()->getDispatcher()->getNumManifolds());
 
 	/// solve all the constraints for this island
-	m_islandManager->buildAndProcessIslands(getCollisionWorld()->getDispatcher(),getCollisionWorld(),m_solverIslandCallback);
+    m_islandManager->buildAndProcessIslands( getCollisionWorld()->getDispatcher(), getCollisionWorld(), m_constraints, m_solverIslandCallback );
 
-	m_solverIslandCallback->processConstraints();
+	//m_solverIslandCallback->processConstraints();
 
 	m_constraintSolver->allSolved(solverInfo, m_debugDrawer);
 }
