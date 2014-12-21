00480e44b181f0c62ad18d15be12becc01d061a7
George Papadopoulos <virusfree21@gmail.com>
2014-12-21 19:50:25 +0200

    add DebugDraw for convex shaped that are wrapped with a btUniformScalingShape

--- src/BulletCollision/CollisionDispatch/btCollisionWorld.cpp
+++ src/BulletCollision/CollisionDispatch/btCollisionWorld.cpp
@@ -23,6 +23,7 @@ subject to the following restrictions:
 #include "BulletCollision/CollisionShapes/btBvhTriangleMeshShape.h" //for raycasting
 #include "BulletCollision/NarrowPhaseCollision/btRaycastCallback.h"
 #include "BulletCollision/CollisionShapes/btCompoundShape.h"
+#include "BulletCollision/CollisionShapes/btUniformScalingShape.h"
 #include "BulletCollision/NarrowPhaseCollision/btSubSimplexConvexCast.h"
 #include "BulletCollision/NarrowPhaseCollision/btGjkConvexCast.h"
 #include "BulletCollision/NarrowPhaseCollision/btContinuousConvexCollision.h"
@@ -1338,6 +1339,17 @@ void btCollisionWorld::debugDrawObject(const btTransform& worldTransform, const 
                 break;
 
             }
+
+		case UNIFORM_SCALING_SHAPE_PROXYTYPE:
+		{
+				const btUniformScalingShape* uniformScaleShape = static_cast<const btUniformScalingShape*>(shape);
+				btScalar scale = uniformScaleShape->getUniformScalingFactor();
+				btTransform childTrans = btTransform(btMatrix3x3(scale, 0, 0, 0, scale, 0, 0, 0, scale));
+				const btCollisionShape* colShape = uniformScaleShape->getChildShape();
+				debugDrawObject(worldTransform*childTrans, colShape, color);
+			break;
+		}
+
         default:
             {
 
