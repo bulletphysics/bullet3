e66be7a7efef54cb7c00b0ecfbf1802969da75db
Andrew Meadows <andrew@highfidelity.io>
2015-12-03 22:00:52 -0800

    replaced spaced indentation with tabs

--- build3/premake4.lua
+++ build3/premake4.lua
@@ -1,5 +1,5 @@
 
-    solution "0_Bullet3Solution"
+	solution "0_Bullet3Solution"
 
 	local osversion = os.getversion()
 	print(string.format(" %d.%d.%d (%s)",
@@ -14,26 +14,26 @@
 
 	act = ""
 
-    if _ACTION then
-        act = _ACTION
-    end
+	if _ACTION then
+		act = _ACTION
+	end
 
-    newoption {
-        trigger     = "ios",
-        description = "Enable iOS target (requires xcode4)"
+	newoption {
+		trigger     = "ios",
+		description = "Enable iOS target (requires xcode4)"
 	}
 
 	newoption
-    {
-        trigger = "force_dlopen_opengl",
-        description = "Dynamically load OpenGL (instead of static/dynamic linking)"
-    }
+	{
+		trigger = "force_dlopen_opengl",
+		description = "Dynamically load OpenGL (instead of static/dynamic linking)"
+	}
 
 	newoption
-    {
-        trigger = "force_dlopen_x11",
-        description = "Dynamically load OpenGL (instead of static/dynamic linking)"
-    }
+	{
+		trigger = "force_dlopen_x11",
+		description = "Dynamically load OpenGL (instead of static/dynamic linking)"
+	}
 
 	newoption
 	{
@@ -49,17 +49,17 @@
 
 --	--_OPTIONS["midi"] = "1";
 
-    newoption
-    {
-        trigger = "no-demos",
-        description = "Don't build demos"
-    }
+	newoption
+	{
+		trigger = "no-demos",
+		description = "Don't build demos"
+	}
 
-    newoption
-    {
-        trigger = "no-extras",
-        description = "Don't build Extras"
-    }
+	newoption
+	{
+		trigger = "no-extras",
+		description = "Don't build Extras"
+	}
 
 	newoption
 	{
@@ -74,10 +74,10 @@
 	}
 
 	newoption {
-        trigger     = "targetdir",
-        value       = "path such as ../bin",
-        description = "Set the output location for the generated project files"
-    }
+		trigger     = "targetdir",
+		value       = "path such as ../bin",
+		description = "Set the output location for the generated project files"
+	}
 
 	newoption
 	{
@@ -91,11 +91,11 @@
 		description = "Disable unit tests using gtest"
 	}
 
-    newoption
-    {
-        trigger = "no-bullet3",
-        description = "Do not build bullet3 libs"
-    }
+	newoption
+	{
+		trigger = "no-bullet3",
+		description = "Do not build bullet3 libs"
+	}
 
 	configurations {"Release", "Debug"}
 	configuration "Release"
@@ -130,25 +130,25 @@
 	postfix=""
 
 	if _ACTION == "xcode4" then
-        if _OPTIONS["ios"] then
-            _OPTIONS["no-bullet3"] = "1"
-            _OPTIONS["no-gtest"] = "1"
+		if _OPTIONS["ios"] then
+			_OPTIONS["no-bullet3"] = "1"
+			_OPTIONS["no-gtest"] = "1"
 
-            postfix = "ios";
-            xcodebuildsettings
-            {
-                'INFOPLIST_FILE = "../../test/Bullet2/Info.plist"',
-                'CODE_SIGN_IDENTITY = "iPhone Developer"',
-                "SDKROOT = iphoneos",
-                'ARCHS = "armv7"',
-                'TARGETED_DEVICE_FAMILY = "1,2"',
-                'VALID_ARCHS = "armv7"',
-            }
+			postfix = "ios";
+			xcodebuildsettings
+			{
+				'INFOPLIST_FILE = "../../test/Bullet2/Info.plist"',
+				'CODE_SIGN_IDENTITY = "iPhone Developer"',
+				"SDKROOT = iphoneos",
+				'ARCHS = "armv7"',
+				'TARGETED_DEVICE_FAMILY = "1,2"',
+				'VALID_ARCHS = "armv7"',
+			}
 		else
 			xcodebuildsettings
 			{
-        		'ARCHS = "$(ARCHS_STANDARD_32_BIT) $(ARCHS_STANDARD_64_BIT)"',
-        		'VALID_ARCHS = "x86_64 i386"',
+				'ARCHS = "$(ARCHS_STANDARD_32_BIT) $(ARCHS_STANDARD_64_BIT)"',
+				'VALID_ARCHS = "x86_64 i386"',
 --			'SDKROOT = "macosx10.9"',
 			}
 		end
@@ -176,73 +176,73 @@
 
 	language "C++"
 
-    if _OPTIONS["no-bullet3"] then
-        print "--no-bullet3 implies --no-demos"
-        _OPTIONS["no-demos"] = "1"
-    else 
-        include "../src/Bullet3Common"
-        include "../src/Bullet3Geometry"
-        include "../src/Bullet3Collision"
-        include "../src/Bullet3Dynamics"
-        include "../src/Bullet3OpenCL"
-        include "../src/Bullet3Serialize/Bullet2FileLoader"
-    end
+	if _OPTIONS["no-bullet3"] then
+		print "--no-bullet3 implies --no-demos"
+		_OPTIONS["no-demos"] = "1"
+	else
+		include "../src/Bullet3Common"
+		include "../src/Bullet3Geometry"
+		include "../src/Bullet3Collision"
+		include "../src/Bullet3Dynamics"
+		include "../src/Bullet3OpenCL"
+		include "../src/Bullet3Serialize/Bullet2FileLoader"
+	end
 
-    if _OPTIONS["no-extras"] then
-        print "--no-extras implies --no-demos"
-        _OPTIONS["no-demos"] = "1"
-    else
-        include "../Extras"
-    end
+	if _OPTIONS["no-extras"] then
+		print "--no-extras implies --no-demos"
+		_OPTIONS["no-demos"] = "1"
+	else
+		include "../Extras"
+	end
 
-    if not _OPTIONS["no-demos"] then
-        include "../examples/ExampleBrowser"
-        include "../examples/OpenGLWindow"
-        include "../examples/ThirdPartyLibs/Gwen"
+	if not _OPTIONS["no-demos"] then
+		include "../examples/ExampleBrowser"
+		include "../examples/OpenGLWindow"
+		include "../examples/ThirdPartyLibs/Gwen"
 
-        include "../examples/HelloWorld"
-        include "../examples/BasicDemo"
+		include "../examples/HelloWorld"
+		include "../examples/BasicDemo"
 
-        include "../examples/SharedMemory"
-        include "../examples/MultiThreading"
+		include "../examples/SharedMemory"
+		include "../examples/MultiThreading"
 
-        if _OPTIONS["lua"] then
-           include "../examples/ThirdPartyLibs/lua-5.2.3"
-        end
+		if _OPTIONS["lua"] then
+		   include "../examples/ThirdPartyLibs/lua-5.2.3"
+		end
 
-        if not _OPTIONS["no-test"] then
-            include "../test/SharedMemory"
-            if _OPTIONS["enet"] then
-                include "../examples/ThirdPartyLibs/enet"
-                include "../test/enet/client"
-                include "../test/enet/server"
-            end
-        end
-    end
+		if not _OPTIONS["no-test"] then
+			include "../test/SharedMemory"
+			if _OPTIONS["enet"] then
+				include "../examples/ThirdPartyLibs/enet"
+				include "../test/enet/client"
+				include "../test/enet/server"
+			end
+		end
+	end
 
-    if not _OPTIONS["no-test"] then
-	    include "../test/Bullet2"
+	if not _OPTIONS["no-test"] then
+		include "../test/Bullet2"
 
-        if not _OPTIONS["no-gtest"] then
-            include "../test/gtest-1.7.0"
---            include "../test/hello_gtest"
-            include "../test/collision"
-            if not _OPTIONS["no-bullet3"] then
-                if not _OPTIONS["no-extras"] then
-                    include "../test/InverseDynamics"
-                end
-                include "../test/TestBullet3OpenCL"
-            end
-            if not _OPTIONS["no-demos"] then
-                -- Gwen is only used for demos
-                include "../test/GwenOpenGLTest"
-            end
-        end
-    end
+		if not _OPTIONS["no-gtest"] then
+			include "../test/gtest-1.7.0"
+--			include "../test/hello_gtest"
+			include "../test/collision"
+			if not _OPTIONS["no-bullet3"] then
+				if not _OPTIONS["no-extras"] then
+					include "../test/InverseDynamics"
+				end
+				include "../test/TestBullet3OpenCL"
+			end
+			if not _OPTIONS["no-demos"] then
+				-- Gwen is only used for demos
+				include "../test/GwenOpenGLTest"
+			end
+		end
+	end
 
 	include "../src/BulletInverseDynamics"
  	include "../src/BulletSoftBody"
-    include "../src/BulletDynamics"
-    include "../src/BulletCollision"
-    include "../src/LinearMath"
+	include "../src/BulletDynamics"
+	include "../src/BulletCollision"
+	include "../src/LinearMath"
 
