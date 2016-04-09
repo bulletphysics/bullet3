6b0bd0356b41df22f8f0d06a3c74523cd58316d4
Grant Kim <enpinion@gmail.com>
2016-04-09 14:20:43 +0900

    modified:   examples/ThirdPartyLibs/Gwen/Macros.h 	modified:   src/Bullet3Common/b3FileUtils.h

--- src/Bullet3Common/b3FileUtils.h
+++ src/Bullet3Common/b3FileUtils.h
@@ -36,7 +36,7 @@ struct b3FileUtils
 
 			for (int i=0;!f && i<numPrefixes;i++)
 			{
-#ifdef _WIN32
+#if defined(_WIN32) && ( !defined(__MINGW32__) || !defined(__MINGW64__) )
 				sprintf_s(relativeFileName,maxRelativeFileNameMaxLen,"%s%s",prefix[i],orgFileName);
 #else
 				sprintf(relativeFileName,"%s%s",prefix[i],orgFileName);
