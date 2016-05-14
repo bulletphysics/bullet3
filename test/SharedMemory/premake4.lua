20e39d0abf49260f143ff4dc13bfb1309841f986
Erwin Coumans <erwincoumans@google.com>
2016-05-13 19:01:40 -0700

    fix premake build

--- test/SharedMemory/premake4.lua
+++ test/SharedMemory/premake4.lua
@@ -161,7 +161,7 @@ project ("Test_PhysicsServerInProcessExampleBrowser")
 --		}
 	hasCL = findOpenCL("clew")
 
-	links{"BulletExampleBrowserLib","gwen", "OpenGL_Window","BulletSoftBody", "BulletInverseDynamicsUtils", "BulletInverseDynamics", "BulletDynamics","BulletCollision","LinearMath","Bullet3Common"}
+	links{"BulletExampleBrowserLib","gwen", "OpenGL_Window","BulletFileLoader","BulletWorldImporter","BulletSoftBody", "BulletInverseDynamicsUtils", "BulletInverseDynamics", "BulletDynamics","BulletCollision","LinearMath","Bullet3Common"}
 	initOpenGL()
 	initGlew()
 
@@ -188,7 +188,49 @@ project ("Test_PhysicsServerInProcessExampleBrowser")
 
 		files {
 			"test.c",
-			"../../examples/ExampleBrowser/ExampleEntries.cpp",
+			"../../examples/ExampleBrowser/InProcessExampleBrowser.cpp",
+			"../../examples/SharedMemory/InProcessMemory.cpp",
+			"../../examples/SharedMemory/PhysicsClient.cpp",
+                        "../../examples/SharedMemory/PhysicsClient.h",
+                        "../../examples/SharedMemory/PhysicsServer.cpp",
+                        "../../examples/SharedMemory/PhysicsServer.h",
+			"../../examples/SharedMemory/PhysicsServerExample.cpp",
+			"../../examples/SharedMemory/SharedMemoryInProcessPhysicsC_API.cpp",
+                        "../../examples/SharedMemory/PhysicsServerSharedMemory.cpp",
+                        "../../examples/SharedMemory/PhysicsServerSharedMemory.h",
+                        "../../examples/SharedMemory/PhysicsDirect.cpp",
+                        "../../examples/SharedMemory/PhysicsDirect.h",
+                        "../../examples/SharedMemory/PhysicsDirectC_API.cpp",
+                        "../../examples/SharedMemory/PhysicsDirectC_API.h",
+                        "../../examples/SharedMemory/PhysicsServerCommandProcessor.cpp",
+                        "../../examples/SharedMemory/PhysicsServerCommandProcessor.h",
+                        "../../examples/SharedMemory/PhysicsClientSharedMemory.cpp",
+                        "../../examples/SharedMemory/PhysicsClientSharedMemory.h",
+                        "../../examples/SharedMemory/PhysicsClientC_API.cpp",
+                        "../../examples/SharedMemory/PhysicsClientC_API.h",
+                        "../../examples/SharedMemory/Win32SharedMemory.cpp",
+                        "../../examples/SharedMemory/Win32SharedMemory.h",
+                        "../../examples/SharedMemory/PosixSharedMemory.cpp",
+                        "../../examples/SharedMemory/PosixSharedMemory.h",
+                        "../../examples/Utils/b3ResourcePath.cpp",
+                        "../../examples/Utils/b3ResourcePath.h",
+                        "../../examples/ThirdPartyLibs/tinyxml/tinystr.cpp",
+                        "../../examples/ThirdPartyLibs/tinyxml/tinyxml.cpp",
+                        "../../examples/ThirdPartyLibs/tinyxml/tinyxmlerror.cpp",
+                        "../../examples/ThirdPartyLibs/tinyxml/tinyxmlparser.cpp",
+                        "../../examples/ThirdPartyLibs/Wavefront/tiny_obj_loader.cpp",
+                        "../../examples/ThirdPartyLibs/Wavefront/tiny_obj_loader.h",
+                        "../../examples/Importers/ImportColladaDemo/LoadMeshFromCollada.cpp",
+                        "../../examples/Importers/ImportObjDemo/LoadMeshFromObj.cpp",
+                        "../../examples/Importers/ImportObjDemo/Wavefront2GLInstanceGraphicsShape.cpp",
+                        "../../examples/Importers/ImportURDFDemo/BulletUrdfImporter.cpp",
+                        "../../examples/Importers/ImportURDFDemo/MyMultiBodyCreator.cpp",
+                        "../../examples/Importers/ImportURDFDemo/URDF2Bullet.cpp",
+                        "../../examples/Importers/ImportURDFDemo/UrdfParser.cpp",
+                        "../../examples/Importers/ImportURDFDemo/urdfStringSplit.cpp",
+			"../../examples/MultiThreading/b3PosixThreadSupport.cpp",
+			"../../examples/MultiThreading/b3Win32ThreadSupport.cpp",
+                "../../examples/MultiThreading/b3ThreadSupportInterface.cpp",
 			}
 	if os.is("Linux") then
        		initX11()
