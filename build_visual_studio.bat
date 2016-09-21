5c747159271665ee62859470145f9099067cd30f
erwincoumans <erwin.coumans@gmail.com>
2016-09-20 21:18:55 -0700

    Update build_visual_studio.bat
    remove some clutter form the .bat file

--- build_visual_studio.bat
+++ build_visual_studio.bat
@@ -1,22 +1,6 @@
 
-rem premake4 --with-pe  vs2010
-rem premake4  --bullet2demos vs2010
 cd build3
 premake4   --enable_openvr  --targetdir="../bin" vs2010 
 
-rem premake4   --enable_pybullet --python_include_dir="C:/Python-3.6.0a3/include" --python_lib_dir="C:/Python-3.6.0a3/libs" --enable_openvr  --targetdir="../bin" vs2010 
-rem premake4   --enable_openvr --enable_pybullet --targetdir="../bin" vs2010 
-rem premake4   --targetdir="../server2bin" vs2010 
-rem cd vs2010
-rem rename 0_Bullet3Solution.sln 0_server.sln
-rem cd ..
-rem rename vs2010 vs2010_server
-rem 
-rem premake4   --targetdir="../client2bin" vs2010 
-rem cd vs2010
-rem rename 0_Bullet3Solution.sln 0_client.sln
-rem cd ..
-rem rename vs2010 vs2010_client
-rem start vs2010/0_Bullet3Solution.sln
-
+rem premake4   --double --enable_pybullet --python_include_dir="C:/Python-3.6.0a3/include" --python_lib_dir="C:/Python-3.6.0a3/libs" --enable_openvr  --targetdir="../bin" vs2010 
 pause
