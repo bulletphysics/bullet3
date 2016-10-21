d309e298e161afc8e0b07e6bbef8286dbea09827
erwincoumans <erwin.coumans@gmail.com>
2016-10-20 17:11:33 -0700

    Update fontstash.cpp
    use free instead of 'delete' for the tex->m_texels memory

--- examples/OpenGLWindow/fontstash.cpp
+++ examples/OpenGLWindow/fontstash.cpp
@@ -793,7 +793,7 @@ void sth_delete(struct sth_stash* stash)
 	tex = stash->textures;
 	while(tex != NULL) {
 		curtex = tex;
-		delete tex->m_texels;
+		free(tex->m_texels);
 		tex->m_texels=0;
 		tex = tex->next;
 		stash->m_renderCallbacks->updateTexture(curtex,0,0,0);
