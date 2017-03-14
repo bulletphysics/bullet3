f159998220cdb521cc53abbf36585e10a2d5c039
Erik Ogenvik <erik@ogenvik.org>
2017-03-14 22:07:06 +0100

    Added test for btKinematicCharacterController.
    For now just a simple instantiation unit test.

---
+++ test/BulletDynamics/test_btKinematicCharacterController.cpp
@@ -0,0 +1,27 @@
+
+
+#include <btBulletDynamicsCommon.h>
+#include <BulletDynamics/Character/btKinematicCharacterController.h>
+#include <gtest/gtest.h>
+#include <BulletCollision/CollisionDispatch/btGhostObject.h>
+
+
+GTEST_TEST(BulletDynamics, KinematicCharacterController) {
+
+    btPairCachingGhostObject* ghostObject = new btPairCachingGhostObject();
+    btBoxShape* convexShape = new btBoxShape(btVector3(1, 1, 1));
+
+    //For now only a simple test that it initializes correctly.
+    btKinematicCharacterController* tested = new btKinematicCharacterController(ghostObject, convexShape, 1);
+    EXPECT_TRUE(tested);
+
+    EXPECT_FLOAT_EQ(-9.8 * 3.0, tested->getGravity().x());
+    EXPECT_FLOAT_EQ(0, tested->getGravity().y());
+    EXPECT_FLOAT_EQ(0, tested->getGravity().z());
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
