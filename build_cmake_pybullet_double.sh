ade68a6a86837329a88ff120cfea7da167d0dd87
Julian <yg1811@hotmail.com>
2017-07-01 22:38:40 -0700

    now impolement camera and recording

--- build_cmake_pybullet_double.sh
+++ build_cmake_pybullet_double.sh
@@ -2,7 +2,7 @@
 rm CMakeCache.txt
 mkdir build_cmake
 cd build_cmake
-cmake -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=OFF -DUSE_DOUBLE_PRECISION=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=ON -DUSE_DOUBLE_PRECISION=ON -DCMAKE_BUILD_TYPE=Release ..
 make -j12
 cd examples
 cd pybullet
