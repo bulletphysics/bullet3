cc0c46c4b0eb5b82ae185687a0eb06da8659966d
Erwin Coumans <erwin.coumans@gmail.com>
2017-09-21 14:24:22 -0700

    re-update the VR camera

--- examples/StandaloneMain/hellovr_opengl_main.cpp
+++ examples/StandaloneMain/hellovr_opengl_main.cpp
@@ -1804,6 +1804,8 @@ void CMainApplication::RenderStereoTargets()
 		m_app->m_instancingRenderer->getActiveCamera()->setCameraUpVector(mat[0],mat[1],mat[2]);
 		m_app->m_instancingRenderer->getActiveCamera()->setVRCamera(viewMatLeft.get(),m_mat4ProjectionLeft.get());
 		m_app->m_instancingRenderer->updateCamera(m_app->getUpAxis());
+		m_app->m_instancingRenderer->getActiveCamera()->setVRCamera(viewMatLeft.get(),m_mat4ProjectionLeft.get());
+
 	}
 
 	glBindFramebuffer( GL_FRAMEBUFFER, leftEyeDesc.m_nRenderFramebufferId );
@@ -1862,6 +1864,7 @@ void CMainApplication::RenderStereoTargets()
 		Matrix4 viewMatRight = m_mat4eyePosRight * m_mat4HMDPose * rotYtoZ;
 		m_app->m_instancingRenderer->getActiveCamera()->setVRCamera(viewMatRight.get(),m_mat4ProjectionRight.get());
 		m_app->m_instancingRenderer->updateCamera(m_app->getUpAxis());
+		m_app->m_instancingRenderer->getActiveCamera()->setVRCamera(viewMatRight.get(),m_mat4ProjectionRight.get());
 	}
 	
 	glBindFramebuffer( GL_FRAMEBUFFER, rightEyeDesc.m_nRenderFramebufferId );
