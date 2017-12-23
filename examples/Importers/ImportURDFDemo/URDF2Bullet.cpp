cb2fcfd032c1a86ed4588014507e9326ce3c3fe8
Adrià Garriga <adria.garriga@gmail.com>
2017-12-23 17:56:47 +0100

    Skipping calculateLocalInertia is unnecessary

--- examples/Importers/ImportURDFDemo/URDF2Bullet.cpp
+++ examples/Importers/ImportURDFDemo/URDF2Bullet.cpp
@@ -296,9 +296,7 @@ void ConvertURDF2BulletInternal(
          */
         if (mass)
         {
-            if (!(flags & CUF_USE_URDF_INERTIA) && (localInertiaDiagonal[0] == 0.0 ||
-													localInertiaDiagonal[1] == 0.0 ||
-													localInertiaDiagonal[2] == 0.0))
+            if (!(flags & CUF_USE_URDF_INERTIA))
             {
                 compoundShape->calculateLocalInertia(mass, localInertiaDiagonal);
                 btAssert(localInertiaDiagonal[0] < 1e10);
