e2105dfaad7782405b0a3be84531465d9bc59d13
Will Rosecrans <wrosecran@edgecast.com>
2018-01-08 15:00:20 -0800

    Fix nproc unavailable

--- build_cmake_pybullet_double.sh
+++ build_cmake_pybullet_double.sh
@@ -6,7 +6,7 @@ fi
 mkdir -p build_cmake
 cd build_cmake
 cmake -DBUILD_PYBULLET=ON -DBUILD_PYBULLET_NUMPY=OFF -DUSE_DOUBLE_PRECISION=ON -DCMAKE_BUILD_TYPE=Release ..
-make -j $(command nproc || echo 12)
+make -j $(command nproc 2>/dev/null || echo 12)
 cd examples
 cd pybullet
 if [ -e pybullet.dylib ]; then
