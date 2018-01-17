11e1cfeed64adfd40bed30e4cb4181e6ad4955b5
Niadb <34917535+Niadb@users.noreply.github.com>
2018-01-16 23:14:32 -0700

    Add files via upload
     There doesn't seem to be a need to applyDamping and predictIntegratedTransform for bodies that are not active.
    
    Profiling showed my app was spending 2/7 of physics update time in here despite most objects being inactive. 
    
    If you prefer a flag to turn this on/off let me know

--- src/BulletDynamics/Dynamics/btDiscreteDynamicsWorld.cpp
+++ src/BulletDynamics/Dynamics/btDiscreteDynamicsWorld.cpp
@@ -1146,7 +1146,7 @@ void	btDiscreteDynamicsWorld::predictUnconstraintMotion(btScalar timeStep)
 	for ( int i=0;i<m_nonStaticRigidBodies.size();i++)
 	{
 		btRigidBody* body = m_nonStaticRigidBodies[i];
-		if (!body->isStaticOrKinematicObject())
+		if (!body->isStaticOrKinematicObject() && body->isActive())
 		{
 			//don't integrate/update velocities here, it happens in the constraint solver
 
