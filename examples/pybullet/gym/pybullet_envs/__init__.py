6e4df1602e80054528d7dd9a1d516e891fc0fd65
Ishaan Varshney <ishaan.varshney@gmail.com>
2018-02-01 17:24:48 +1100

    unknown character causing syntax error

--- examples/pybullet/gym/pybullet_envs/__init__.py
+++ examples/pybullet/gym/pybullet_envs/__init__.py
@@ -1,7 +1,7 @@
 import gym
 from gym.envs.registration import registry, make, spec
 def register(id,*args,**kvargs):
-	if id in  registry.env_specs:
+	if id in registry.env_specs:
 		return
 	else:
 		return gym.envs.registration.register(id,*args,**kvargs)
