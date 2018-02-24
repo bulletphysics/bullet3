e4d2591f94321a75d5b5efbc93a42635deb15f0c
Nickolai Belakovski <nickolai.belakovski@nio.io>
2018-02-23 20:30:42 -0800

    Added serialization and deserialization of m_localtime

--- src/BulletDynamics/Dynamics/btDiscreteDynamicsWorld.cpp
+++ src/BulletDynamics/Dynamics/btDiscreteDynamicsWorld.cpp
@@ -1483,6 +1483,8 @@ void	btDiscreteDynamicsWorld::serializeDynamicsWorldInfo(btSerializer* serialize
 
 		memset(worldInfo ,0x00,len);
 
+		worldInfo->m_localTime = m_localTime;
+
 		m_gravity.serialize(worldInfo->m_gravity);
 		worldInfo->m_solverInfo.m_tau = getSolverInfo().m_tau;
 		worldInfo->m_solverInfo.m_damping = getSolverInfo().m_damping;
