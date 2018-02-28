1b045a8e4f7af57a7bf08086c71ae54c7da245f2
Giuseppe Barbieri <elect86@gmail.com>
2018-02-28 09:13:34 +0100

    Update CommonRigidBodyBase.h

--- examples/CommonInterfaces/CommonRigidBodyBase.h
+++ examples/CommonInterfaces/CommonRigidBodyBase.h
@@ -58,7 +58,7 @@ struct CommonRigidBodyBase : public CommonExampleInterface
 		m_collisionConfiguration = new btDefaultCollisionConfiguration();
 		//m_collisionConfiguration->setConvexConvexMultipointIterations();
 
-		///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+		///use the default collision dispatcher. For parallel processing you can use a different dispatcher (see Extras/BulletMultiThreaded)
 		m_dispatcher = new	btCollisionDispatcher(m_collisionConfiguration);
 
 		m_broadphase = new btDbvtBroadphase();
