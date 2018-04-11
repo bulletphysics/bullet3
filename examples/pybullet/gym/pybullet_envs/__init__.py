ac945df07ae59f804ffcd9ec4a39cf48f7a1156c
erwincoumans <erwin.coumans@gmail.com>
2018-04-11 10:14:24 -0700

    remote 'render' arg

--- examples/pybullet/gym/pybullet_envs/__init__.py
+++ examples/pybullet/gym/pybullet_envs/__init__.py
@@ -33,7 +33,6 @@ register(
 register(
     id='MinitaurReactiveEnv-v0',
     entry_point='pybullet_envs.minitaur.envs:MinitaurReactiveEnv',
-    render=True,
     timestep_limit=1000,
     reward_threshold=5.0,
 )
