90939279f9b9318a1ee37029befdedff808c63bc
Erwin Coumans <erwincoumans@erwincoumans-macbookpro2.roam.corp.google.com>
2018-05-20 14:18:53 +1000

    PyBullet: cmake build, enable DBUILD_PYBULLET_NUMPY=ON fix issue with HalfCheetahBulletEnv-v0 in previous commit

--- examples/pybullet/gym/pybullet_envs/robot_locomotors.py
+++ examples/pybullet/gym/pybullet_envs/robot_locomotors.py
@@ -116,8 +116,8 @@ class HalfCheetah(WalkerBase):
 		# Use contact other than feet to terminate episode: due to a lot of strange walks using knees
 		return +1 if np.abs(pitch) < 1.0 and not self.feet_contact[1] and not self.feet_contact[2] and not self.feet_contact[4] and not self.feet_contact[5] else -1
 
-	def robot_specific_reset(self):
-		WalkerBase.robot_specific_reset(self)
+	def robot_specific_reset(self, bullet_client):
+		WalkerBase.robot_specific_reset(self, bullet_client)
 		self.jdict["bthigh"].power_coef = 120.0
 		self.jdict["bshin"].power_coef  = 90.0
 		self.jdict["bfoot"].power_coef  = 60.0
