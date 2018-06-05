e4cd88e24f0238de392f13d2a5c62e2f9cf73661
Erwin Coumans <erwin.coumans@gmail.com>
2018-06-05 11:36:54 +1000

    PyBullet: allow to choose the shared memory key when starting SHARED_MEMORY_SERVER

--- examples/pybullet/pybullet.c
+++ examples/pybullet/pybullet.c
@@ -399,7 +399,7 @@ static PyObject* pybullet_connectPhysicsServer(PyObject* self, PyObject* args, P
 			}
             case eCONNECT_SHARED_MEMORY_SERVER:
             {
-                sm = b3CreateInProcessPhysicsServerFromExistingExampleBrowserAndConnect2(0);
+                sm = b3CreateInProcessPhysicsServerFromExistingExampleBrowserAndConnect2(0, key);
                 break;
             }
 			case eCONNECT_DIRECT:
