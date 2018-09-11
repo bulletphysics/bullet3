5b1588fc3f7c19a6db92e1a00e39c5dc158efbed
erwincoumans <erwin.coumans@gmail.com>
2018-09-10 23:32:51 -0700

    fix createProtobufs.bat script

--- examples/SharedMemory/grpc/proto/createProtobufs.bat
+++ examples/SharedMemory/grpc/proto/createProtobufs.bat
@@ -6,6 +6,9 @@ del pybullet.grpc.pb.h
 ..\..\..\ThirdPartyLibs\grpc\lib\win32\protoc --proto_path=. --cpp_out=. pybullet.proto
 ..\..\..\ThirdPartyLibs\grpc\lib\win32\protoc.exe --plugin=protoc-gen-grpc="..\..\..\ThirdPartyLibs\grpc\lib\win32\grpc_cpp_plugin.exe" --grpc_out=. pybullet.proto
 
+rename pybullet.grpc.pb.cc pybullet.grpc.pb.cpp
+rename pybullet.pb.cc pybullet.pb.cpp
+
 del pybullet_pb2.py
 del pybullet_pb2_grpc.py
 
