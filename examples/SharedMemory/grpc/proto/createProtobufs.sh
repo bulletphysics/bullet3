3b0a44e1be0162c27486fd8b3cc991f7ce69afed
Erwin Coumans <erwincoumans@erwincoumans-macbookpro2.roam.corp.google.com>
2018-09-10 23:24:00 -0700

    fix createProtobufs.sh script

--- examples/SharedMemory/grpc/proto/createProtobufs.sh
+++ examples/SharedMemory/grpc/proto/createProtobufs.sh
@@ -1,19 +1,15 @@
-rm ../pybullet.pb.cpp
-rm ../pybullet.pb.h
-rm ../pybullet.grpc.pb.cpp
-rm ../pybullet.grpc.pb.h
+rm pybullet.pb.cpp
+rm pybullet.pb.h
+rm pybullet.grpc.pb.cpp
+rm pybullet.grpc.pb.h
 
 protoc --proto_path=. --cpp_out=. pybullet.proto
 protoc --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` --grpc_out=. pybullet.proto
-mv pybullet.grpc.pb.cc ../pybullet.grpc.pb.cpp
-mv pybullet.grpc.pb.h ../pybullet.grpc.pb.h
-mv pybullet.pb.cc ../pybullet.pb.cpp
-mv pybullet.pb.h ../pybullet.pb.h
+mv pybullet.grpc.pb.cc pybullet.grpc.pb.cpp
+mv pybullet.pb.cc pybullet.pb.cpp
 
-rm ../pybullet_pb2.py
-rm ../pybullet_pb2_grpc.py
+rm pybullet_pb2.py
+rm pybullet_pb2_grpc.py
 
 protoc --proto_path=. --python_out=. pybullet.proto
 python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. pybullet.proto
-mv pybullet_pb2.py ../pybullet_pb2.py
-mv pybullet_pb2_grpc.py ../pybullet_pb2_grpc.py 
