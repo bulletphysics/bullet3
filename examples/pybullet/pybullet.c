e793bd22bb700e15c1e0caed48e821338efc3258
xantares <xantares09@hotmail.com>
2019-01-15 10:24:11 +0100

    Cancel built time print changes

--- examples/pybullet/pybullet.c
+++ examples/pybullet/pybullet.c
@@ -10738,9 +10738,7 @@ initpybullet(void)
 	SpamError = PyErr_NewException("pybullet.error", NULL, NULL);
 	Py_INCREF(SpamError);
 	PyModule_AddObject(m, "error", SpamError);
-#ifndef NDEBUG
 	printf("pybullet build time: %s %s\n", __DATE__, __TIME__);
-#endif
 
 	Py_AtExit(b3pybulletExitFunc);
 
