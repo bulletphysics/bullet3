c44c7f2891bf23733cd10db5c913df807884d96c
Chuyuan Fu <fuchuyuan.kelly@gmail.com>
2019-03-25 14:55:02 -0700

    add files

---
+++ examples/BlockSolver/RigidBodyBoxes.h
@@ -0,0 +1,6 @@
+#ifndef BLOCKSOLVER_RIGIDBODYBOXES_H_
+#define BLOCKSOLVER_RIGIDBODYBOXES_H_
+
+class CommonExampleInterface* RigidBodyBoxesCreateFunc(struct CommonExampleOptions& options);
+
+#endif  //BLOCKSOLVER_RIGIDBODYBOXES_H_
