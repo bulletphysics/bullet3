38804a9f0f1e67f852490f253861d20ebdd43315
Erwin Coumans <erwin.coumans@gmail.com>
2019-08-06 10:56:21 -0700

    sync graphics server at resetSimulation, load* and resetJointState

--- examples/SharedMemory/PhysicsServerCommandProcessor.cpp
+++ examples/SharedMemory/PhysicsServerCommandProcessor.cpp
@@ -3234,6 +3234,7 @@ bool PhysicsServerCommandProcessor::processImportedObjects(const char* fileName,
 
 	m_data->m_saveWorldBodyData.push_back(sd);
 
+	syncPhysicsToGraphics2();
 	return loadOk;
 }
 
@@ -9255,6 +9256,8 @@ bool PhysicsServerCommandProcessor::processInitPoseCommand(const struct SharedMe
 		}
 	}
 
+	syncPhysicsToGraphics2();
+
 	SharedMemoryStatus& serverCmd = serverStatusOut;
 	serverCmd.m_type = CMD_CLIENT_COMMAND_COMPLETED;
 
@@ -13034,6 +13037,7 @@ void PhysicsServerCommandProcessor::resetSimulation()
 	notification.m_notificationType = SIMULATION_RESET;
 	m_data->m_pluginManager.addNotification(notification);
 
+	syncPhysicsToGraphics2();
 	
 }
 
