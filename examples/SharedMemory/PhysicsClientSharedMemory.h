3bd4b9ff3c9a16ea66233d3ac52b65324096723a
Tigran Gasparian <gaspariantigran@gmail.com>
2019-08-08 10:53:23 +0200

    .

--- examples/SharedMemory/PhysicsClientSharedMemory.h
+++ examples/SharedMemory/PhysicsClientSharedMemory.h
@@ -15,6 +15,7 @@ protected:
 	void processBodyJointInfo(int bodyUniqueId, const struct SharedMemoryStatus& serverCmd);
 	void resetData();
 	void removeCachedBody(int bodyUniqueId);
+	void clearCachedBodies();
 	virtual void renderSceneInternal(){};
 
 public:
