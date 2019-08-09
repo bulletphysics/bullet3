7adb6fdff3bd182f27668836a5b9d0ecc109856a
Xuchen Han <xuchenhan@xuchenhan-macbookpro.roam.corp.google.com>
2019-08-08 17:43:49 -0700

    2016 -> 2019

--- examples/DeformableDemo/VolumetricDeformable.cpp
+++ examples/DeformableDemo/VolumetricDeformable.cpp
@@ -1,6 +1,6 @@
 /*
  Bullet Continuous Collision Detection and Physics Library
- Copyright (c) 2016 Google Inc. http://bulletphysics.org
+ Copyright (c) 2019 Google Inc. http://bulletphysics.org
  This software is provided 'as-is', without any express or implied warranty.
  In no event will the authors be held liable for any damages arising from the use of this software.
  Permission is granted to anyone to use this software for any purpose,
