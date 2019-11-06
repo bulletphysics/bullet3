9068b7ed912c45e48a877b14a7161de7f93a5563
Xuchen Han <xuchenhan@google.com>
2019-11-05 18:16:13 -0800

    turn off SVD

--- src/BulletSoftBody/btDeformableNeoHookeanForce.h
+++ src/BulletSoftBody/btDeformableNeoHookeanForce.h
@@ -175,7 +175,6 @@ public:
                 btSoftBody::Tetra& tetra = psb->m_tetras[j];
                 btMatrix3x3 P;
                 firstPiola(psb->m_tetraScratches[j],P);
-#define USE_SVD 1
 #ifdef USE_SVD
                 if (max_p > 0)
                 {
