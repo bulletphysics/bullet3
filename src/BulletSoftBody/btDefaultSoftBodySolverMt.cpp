bc1e1bb4456df4212dbb8dd3e094dd76029703ef
Lopuska <anishireche@gmail.com>
2020-01-02 17:40:15 +0100

    Update btDefaultSoftBodySolverMt.cpp

--- src/BulletSoftBody/btDefaultSoftBodySolverMt.cpp
+++ src/BulletSoftBody/btDefaultSoftBodySolverMt.cpp
@@ -22,28 +22,6 @@ subject to the following restrictions:
 #include "BulletSoftBody/btSoftBody.h"
 #include "LinearMath/btThreads.h"
 
-btDefaultSoftBodySolverMt::btDefaultSoftBodySolverMt()
-{
-	// Initial we will clearly need to update solver constants
-	// For now this is global for the cloths linked with this solver - we should probably make this body specific
-	// for performance in future once we understand more clearly when constants need to be updated
-	m_updateSolverConstants = true;
-}
-
-btDefaultSoftBodySolverMt::~btDefaultSoftBodySolverMt()
-{
-}
-
-// In this case the data is already in the soft bodies so there is no need for us to do anything
-void btDefaultSoftBodySolverMt::copyBackToSoftBodies(bool bMove)
-{
-}
-
-void btDefaultSoftBodySolverMt::optimize(btAlignedObjectArray<btSoftBody *> &softBodies, bool forceUpdate)
-{
-	m_softBodySet.copyFromArray(softBodies);
-}
-
 struct SoftBodyUpdater : public btIParallelForBody
 {
 	btAlignedObjectArray<btSoftBody*>* mSoftBodySet;
@@ -70,15 +48,10 @@ void btDefaultSoftBodySolverMt::updateSoftBodies()
 	if (m_softBodySet.size() > 0)
 	{
 		SoftBodyUpdater loop(&m_softBodySet);
-		int grainSize = 10;
+		int grainSize = 5;
 		btParallelFor(0, m_softBodySet.size(), grainSize, loop);
 	}
 }  // updateSoftBodies
-
-bool btDefaultSoftBodySolverMt::checkInitialized()
-{
-	return true;
-}
 
 struct SolveConstraints : public btIParallelForBody
 {
@@ -107,69 +80,10 @@ void btDefaultSoftBodySolverMt::solveConstraints(btScalar solverdt)
 	if (m_softBodySet.size() > 0)
 	{
 		SolveConstraints loop(&m_softBodySet);
-		int grainSize = 10;
+		int grainSize = 5;
 		btParallelFor(0, m_softBodySet.size(), grainSize, loop);
 	}
 }  // btDefaultSoftBodySolverMt::solveConstraints
-
-void btDefaultSoftBodySolverMt::copySoftBodyToVertexBuffer(const btSoftBody *const softBody, btVertexBufferDescriptor *vertexBuffer)
-{
-	// Currently only support CPU output buffers
-	// TODO: check for DX11 buffers. Take all offsets into the same DX11 buffer
-	// and use them together on a single kernel call if possible by setting up a
-	// per-cloth target buffer array for the copy kernel.
-
-	if (vertexBuffer->getBufferType() == btVertexBufferDescriptor::CPU_BUFFER)
-	{
-		const btAlignedObjectArray<btSoftBody::Node> &clothVertices(softBody->m_nodes);
-		int numVertices = clothVertices.size();
-
-		const btCPUVertexBufferDescriptor *cpuVertexBuffer = static_cast<btCPUVertexBufferDescriptor *>(vertexBuffer);
-		float *basePointer = cpuVertexBuffer->getBasePointer();
-
-		if (vertexBuffer->hasVertexPositions())
-		{
-			const int vertexOffset = cpuVertexBuffer->getVertexOffset();
-			const int vertexStride = cpuVertexBuffer->getVertexStride();
-			float *vertexPointer = basePointer + vertexOffset;
-
-			for (int vertexIndex = 0; vertexIndex < numVertices; ++vertexIndex)
-			{
-				btVector3 position = clothVertices[vertexIndex].m_x;
-				*(vertexPointer + 0) = (float)position.getX();
-				*(vertexPointer + 1) = (float)position.getY();
-				*(vertexPointer + 2) = (float)position.getZ();
-				vertexPointer += vertexStride;
-			}
-		}
-		if (vertexBuffer->hasNormals())
-		{
-			const int normalOffset = cpuVertexBuffer->getNormalOffset();
-			const int normalStride = cpuVertexBuffer->getNormalStride();
-			float *normalPointer = basePointer + normalOffset;
-
-			for (int vertexIndex = 0; vertexIndex < numVertices; ++vertexIndex)
-			{
-				btVector3 normal = clothVertices[vertexIndex].m_n;
-				*(normalPointer + 0) = (float)normal.getX();
-				*(normalPointer + 1) = (float)normal.getY();
-				*(normalPointer + 2) = (float)normal.getZ();
-				normalPointer += normalStride;
-			}
-		}
-	}
-}  // btDefaultSoftBodySolverMt::copySoftBodyToVertexBuffer
-
-void btDefaultSoftBodySolverMt::processCollision(btSoftBody *softBody, btSoftBody *otherSoftBody)
-{
-	softBody->defaultCollisionHandler(otherSoftBody);
-}
-
-// For the default solver just leave the soft body to do its collision processing
-void btDefaultSoftBodySolverMt::processCollision(btSoftBody *softBody, const btCollisionObjectWrapper *collisionObjectWrap)
-{
-	softBody->defaultCollisionHandler(collisionObjectWrap);
-}  // btDefaultSoftBodySolverMt::processCollision
 
 struct PredictMotion : public btIParallelForBody
 {
@@ -199,7 +113,7 @@ void btDefaultSoftBodySolverMt::predictMotion(btScalar timeStep)
 	if (m_softBodySet.size() > 0)
 	{
 		PredictMotion loop(&m_softBodySet, timeStep);
-		int grainSize = 10;
+		int grainSize = 5;
 		btParallelFor(0, m_softBodySet.size(), grainSize, loop);
 	}
 }
