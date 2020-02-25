4766586eadf603bc4e413698df829027690901bd
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-25 21:42:25 +0100

    Add option to install extras

--- Extras/BulletRobotics/bullet_robotics.pc.cmake
+++ Extras/BulletRobotics/bullet_robotics.pc.cmake
@@ -1,6 +1,5 @@
 prefix=@BULLET_INSTALL_PREFIX@
-exec_prefix=${prefix}
-libdir=${exec_prefix}/bullet_robotics
+libdir=${prefix}/@BULLET_INSTALL_LIBDIR@
 includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@/bullet_robotics
 
 Name: bullet_robotics
@@ -8,4 +7,4 @@ Description: Bullet extras that include several utilities for robotics including
 Version: @BULLET_VERSION@
 Requires: bullet
 Libs: -L${libdir} -lBulletRobotics
-Cflags: @BULLET_DOUBLE_DEF@ -I${includedir} -I@{prefix}/@BULLET_INSTALL_INCLUDEDIR@ -I@CMAKE_INSTALL_INCLUDEDIR@
+Cflags: @BULLET_DOUBLE_DEF@ -I${includedir} -I${prefix}/@BULLET_INSTALL_INCLUDEDIR@ -I@CMAKE_INSTALL_INCLUDEDIR@
