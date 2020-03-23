9d68d52506620411e4969740eb080c896439f5f4
Aaron Franke <arnfranke@yahoo.com>
2020-03-22 20:00:11 -0400

    Run clang-format as part of Travis CI checks

---
+++ .ci/format.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Format started"
+
+bash ./clang-format-all.sh .
+
+echo "clang-format finished"
+
+git diff > patch.patch
+FILESIZE=$(stat -c%s patch.patch)
+MAXSIZE=5
+
+# If no patch has been generated all is OK, clean up, and exit.
+if (( FILESIZE < MAXSIZE )); then
+    printf "Files in this commit comply with the formatting rules.\n"
+    rm -f patch.patch
+    exit 0
+fi
+
+# A patch has been created, notify the user, clean up, and exit.
+printf "\n*** The following differences were found between the code "
+printf "and the formatting rules:\n\n"
+cat patch.patch
+printf "\n*** Aborting, please fix your commit(s) with 'git commit --amend' or 'git rebase -i <hash>'\n"
+rm -f patch.patch
+exit 1
