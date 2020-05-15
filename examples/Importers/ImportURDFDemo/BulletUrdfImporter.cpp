bad085554c06f9ac24330bce5717fd46fddad95e
Chuyuan Fu <fuchuyuan.kelly@gmail.com>
2020-05-15 14:19:23 -0700

    set collision margin for implicit cynlinder

--- examples/Importers/ImportURDFDemo/BulletUrdfImporter.cpp
+++ examples/Importers/ImportURDFDemo/BulletUrdfImporter.cpp
@@ -623,7 +623,7 @@ btCollisionShape* BulletURDFImporter::convertURDFToCollisionShape(const UrdfColl
 			btScalar height = collision->m_geometry.m_capsuleHeight;
 			btCapsuleShapeZ* capsuleShape = new btCapsuleShapeZ(radius, height);
 			shape = capsuleShape;
-			shape->setMargin(gUrdfDefaultCollisionMargin);
+ 			shape->setMargin(gUrdfDefaultCollisionMargin);
 			break;
 		}
 
@@ -636,6 +636,7 @@ btCollisionShape* BulletURDFImporter::convertURDFToCollisionShape(const UrdfColl
 				btVector3 halfExtents(cylRadius, cylRadius, cylHalfLength);
 				btCylinderShapeZ* cylZShape = new btCylinderShapeZ(halfExtents);
 				shape = cylZShape;
+                                shape->setMargin(gUrdfDefaultCollisionMargin);
 			}
 			else
 			{
