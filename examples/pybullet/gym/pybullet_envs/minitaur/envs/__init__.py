6b38a4cfdb46560f309c77bda1b3e352de514cb6
Eric Yihan Chen <ericyihc@gmail.com>
2020-07-14 17:13:59 -0700

    Fix minitaur logging proto and import

--- examples/pybullet/gym/pybullet_envs/minitaur/envs/__init__.py
+++ examples/pybullet/gym/pybullet_envs/minitaur/envs/__init__.py
@@ -5,3 +5,4 @@ from pybullet_envs.minitaur.envs.minitaur_reactive_env import MinitaurReactiveEn
 from pybullet_envs.minitaur.envs.minitaur_stand_gym_env import MinitaurStandGymEnv
 from pybullet_envs.minitaur.envs.minitaur_trotting_env import MinitaurTrottingEnv
 from pybullet_envs.minitaur.envs.minitaur_four_leg_stand_env import MinitaurFourLegStandEnv
+from pybullet_envs.minitaur.envs.minitaur_extended_env import MinitaurExtendedEnv, MinitaurReactiveEnv
