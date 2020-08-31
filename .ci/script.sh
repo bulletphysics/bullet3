bb1da5b5f50fce595bc44d4054b9ad7d5fad5ff8
erwincoumans <erwincoumans@google.com>
2020-08-31 13:35:03 -0700

    try to fix travis, update inertia for Laikago, reset camera at the right time (#3021)
    * laikago_toes_zup.urdf: fill in inertia values, computed as PyBullet does internally (from mass and collision volumes)
    
    * reset camera in the rendering thread to avoid artifacts
    
    * reset camera in the rendering thread to avoid artifacts
    
    * try to fix travis error

--- .ci/script.sh
+++ .ci/script.sh
@@ -9,7 +9,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" && "$CXX" = "g++" ]]; then
   $SUDO apt-get install -y python3-pip
   $SUDO pip3 install -U wheel
   $SUDO pip3 install -U setuptools
-  $SUDO python3 setup.py install
+  python3 setup.py install --user
   python3 examples/pybullet/unittests/unittests.py --verbose
   python3 examples/pybullet/unittests/userDataTest.py --verbose
   python3 examples/pybullet/unittests/saveRestoreStateTest.py --verbose
