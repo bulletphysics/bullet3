f1cb0bdf506cfb5460738163d9a1fa1899a64cfa
WenlongLu <wenlong.will.lu@gmail.com>
2020-09-15 22:59:08 -0700

    Added hacked Kinematic MultiBody example

---
+++ examples/MultiBody/KinematicMultiBody.h
@@ -0,0 +1,7 @@
+
+#ifndef KINEMATIC_MULTI_BODY_H
+#define KINEMATIC_MULTI_BODY_H
+
+class CommonExampleInterface* KinematicMultiBodyCreateFunc(struct CommonExampleOptions& options);
+
+#endif  //MULTI_DOF_DEMO_H
