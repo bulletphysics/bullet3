ddbe6f7a5ab58484206e5dd395bd59891b1cabd7
WenlongLu <wenlong.will.lu@gmail.com>
2020-11-19 15:41:27 -0800

    remove duplicated cpp file in premake4.lua

--- examples/ExampleBrowser/premake4.lua
+++ examples/ExampleBrowser/premake4.lua
@@ -193,7 +193,6 @@ project "App_BulletExampleBrowser"
 		"../MultiBody/SerialChains.cpp",
 		"../MultiBody/TestJointTorqueSetup.cpp",
 		"../MultiBody/Pendulum.cpp",
-                "../MultiBody/KinematicMultiBodyExample.cpp",
 		"../MultiBody/MultiBodySoftContact.cpp",
 		"../MultiBody/MultiBodyConstraintFeedback.cpp",
 		"../MultiBody/InvertedPendulumPDControl.cpp",
