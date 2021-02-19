371a39f6f2f6e7f7e32c0093303975c6e582a855
WenlongLu <wenlong.will.lu@gmail.com>
2021-02-18 19:28:06 -0800

    use the first channel when multi dof params are not set

--- examples/pybullet/pybullet.c
+++ examples/pybullet/pybullet.c
@@ -3457,6 +3457,10 @@ static PyObject* pybullet_setJointMotorControlMultiDof(PyObject* self, PyObject*
 					b3JointControlSetKpMultiDof(commandHandle, info.m_uIndex,
 																kpArray, kpSize);
 				}
+				else if (kpSize == 0)
+				{
+					b3JointControlSetKp(commandHandle, info.m_uIndex, kpArray[0]);
+				}
 				if (info.m_uSize == targetVelocitySize)
 				{
 					b3JointControlSetDesiredVelocityMultiDof(commandHandle, info.m_uIndex,
@@ -3470,6 +3474,10 @@ static PyObject* pybullet_setJointMotorControlMultiDof(PyObject* self, PyObject*
 				{
 					b3JointControlSetKdMultiDof(commandHandle, info.m_uIndex,
 																kdArray, kdSize);
+				}
+				else if (kdSize == 0)
+				{
+					b3JointControlSetKd(commandHandle, info.m_uIndex, kdArray[0]);
 				}
 				if (info.m_uSize == targetForceSize || targetForceSize == 1)
 				{
