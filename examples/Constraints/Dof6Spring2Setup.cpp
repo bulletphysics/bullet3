89e4849030c58a4cff76ad77cb99b9d418dc44a9
maphysart <maphysart@gmail.com>
2021-03-24 11:10:43 +0800

    dev: clean the code. TODO: continue clean the code.

--- examples/Constraints/Dof6Spring2Setup.cpp
+++ examples/Constraints/Dof6Spring2Setup.cpp
@@ -3,13 +3,6 @@
 
 #include "Dof6Spring2Setup.h"
 
-#include "btBulletDynamicsCommon.h"
-#include "BulletDynamics/ConstraintSolver/btNNCGConstraintSolver.h"
-#include "BulletDynamics/MLCPSolvers/btMLCPSolver.h"
-#include "BulletDynamics/MLCPSolvers/btSolveProjectedGaussSeidel.h"
-#include "BulletDynamics/MLCPSolvers/btLemkeSolver.h"
-#include "BulletDynamics/MLCPSolvers/btDantzigSolver.h"
-
 #include "../Utils/b3ResourcePath.h"
 #include "Bullet3Common/b3FileUtils.h"
 #include "../Importers/ImportObjDemo/LoadMeshFromObj.h"
@@ -30,18 +23,6 @@
 #define M_PI_4 0.785398163397448309616
 #endif
 
-extern float g_additionalBodyMass;
-
-//comment this out to compare with original spring constraint
-#define USE_6DOF2
-#ifdef USE_6DOF2
-#define CONSTRAINT_TYPE btGeneric6DofSpring2Constraint
-#define EXTRAPARAMS
-#else
-#define CONSTRAINT_TYPE btGeneric6DofSpringConstraint
-#define EXTRAPARAMS , true
-#endif
-
 #include "../CommonInterfaces/CommonRigidBodyBase.h"
 #include "../CommonInterfaces/CommonMultiBodyBase.h"
 
@@ -58,14 +39,7 @@ btScalar inline degree2rad(btScalar degree){
     return degree / 180.0f * SIMD_PI;
 }
 
-btScalar inline degreeDiff(const btVector3& a1, const btVector3& a2)
-{
-    btVector3 d1 = btVector3(rad2degree(a1[0]), rad2degree(a1[1]), rad2degree(a1[2]));
-    btVector3 d2 = btVector3(rad2degree(a2[0]), rad2degree(a2[1]), rad2degree(a2[2]));
-    return (d1 - d2).norm();
-}
-
-void getEulerValFromDof(btVector3& eulerVal, const btScalar* q)
+void getEulerValFromQuaternion(btVector3& eulerVal, const btScalar* q)
 {
     btQuaternion qt(q[0], q[1], q[2], q[3]);
     qt.getEulerZYX(eulerVal[2], eulerVal[1], eulerVal[0]);
@@ -79,6 +53,7 @@ btScalar sinOffset(btScalar amp, btScalar T, btScalar phase, btScalar time){
     return amp * sin(time / T * SIMD_2_PI + phase);
 }
 
+// TODO use it in code
 btScalar adjustKs(btScalar ks, btScalar mass, btScalar deltaTime)
 {
     btScalar r = ks;
@@ -90,236 +65,7 @@ btScalar adjustKs(btScalar ks, btScalar mass, btScalar deltaTime)
     return r;
 }
 
-btScalar adjustKd(btScalar kd, btScalar mass, btScalar deltaTime)
-{
-    btScalar r = kd;
-    if (kd * deltaTime > mass)
-    {
-        r = mass / deltaTime;
-    }
-    return r;
-}
-
-btVector3 getSpringImpulse(btScalar ks, btScalar kd, btScalar mass,
-        const btVector3& currPos,  const btVector3& balPos, const btVector3& currVel, float deltaTime)
-{
-    //limit stiffness (the spring should not be sampled faster that the quarter of its angular frequency)
-    btScalar _ks = adjustKs(ks, mass, deltaTime);
-    btScalar _kd = adjustKd(kd, mass, deltaTime);
-
-    btVector3 spring_force = -(currPos - balPos) * _ks;
-    btVector3 damp_force = -currVel * _kd;
-
-    btVector3 impulse = (spring_force + damp_force) * deltaTime;
-    return impulse;
-};
-
-
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-////  spring test
-////
-/////////////////////////////////////////////////////////////
-
-struct Dof6Spring2Setup : public CommonRigidBodyBase
-{
-	struct Dof6Spring2SetupInternalData* m_data;
-	btRigidBody* m_body;
-    btScalar m_time;
-    btVector3 m_balancePos;
-    btScalar m_ks;
-    btScalar m_kd;
-    btScalar m_mass;
-    int m_step;
-
-	Dof6Spring2Setup(struct GUIHelperInterface* helper);
-	virtual ~Dof6Spring2Setup();
-	virtual void initPhysics();
-
-	virtual void stepSimulation(float deltaTime);
-
-	virtual void resetCamera()
-	{
-		float dist = 6;
-		float pitch = -45;
-		float yaw = 0;
-		float targetPos[3] = {0, 0, 0};
-		m_guiHelper->resetCamera(dist, yaw, pitch, targetPos[0], targetPos[1], targetPos[2]);
-	}
-};
-
-struct Dof6Spring2SetupInternalData
-{
-	btRigidBody* m_TranslateSpringBody;
-};
-
-Dof6Spring2Setup::Dof6Spring2Setup(struct GUIHelperInterface* helper)
-	: CommonRigidBodyBase(helper)
-{
-	m_data = new Dof6Spring2SetupInternalData;
-	m_time = btScalar(0.0);
-    m_balancePos = btVector3(0.0, 0.0, 0.0);
-    m_ks = 0.1;
-    m_kd = 0.2;
-    m_mass = btScalar(1.0);
-    m_step = 0;
-}
-Dof6Spring2Setup::~Dof6Spring2Setup()
-{
-	exitPhysics();
-	delete m_data;
-}
-void Dof6Spring2Setup::initPhysics()
-{
-	// Setup the basic world
-
-	m_guiHelper->setUpAxis(1);
-
-	m_collisionConfiguration = new btDefaultCollisionConfiguration();
-	m_dispatcher = new btCollisionDispatcher(m_collisionConfiguration);
-	btVector3 worldAabbMin(-10000, -10000, -10000);
-	btVector3 worldAabbMax(10000, 10000, 10000);
-	m_broadphase = new btAxisSweep3(worldAabbMin, worldAabbMax);
-
-	/////// uncomment the corresponding line to test a solver.
-	//m_solver = new btSequentialImpulseConstraintSolver;
-	m_solver = new btNNCGConstraintSolver;
-
-	m_dynamicsWorld = new btDiscreteDynamicsWorld(m_dispatcher, m_broadphase, m_solver, m_collisionConfiguration);
-	m_dynamicsWorld->getDispatchInfo().m_useContinuous = true;
-	m_guiHelper->createPhysicsDebugDrawer(m_dynamicsWorld);
-
-	m_dynamicsWorld->setGravity(btVector3(0, 0, 0));
-
-	m_dynamicsWorld->getSolverInfo().m_numIterations = 100;
-
-	btCollisionShape* shape;
-	btVector3 localInertia(0, 0, 0);
-	btDefaultMotionState* motionState;
-	btTransform bodyTransform;
-	btScalar mass;
-	btTransform localA;
-	btTransform localB;
-	CONSTRAINT_TYPE* constraint;
-
-	float z_offset = 2.0;
-	// this static body is used in the constraint calculation
-	//static body centered in the origo
-	mass = 0.0;
-	shape = new btBoxShape(btVector3(0.5, 0.5, 0.5));
-	localInertia = btVector3(0, 0, 0);
-	bodyTransform.setIdentity();
-	bodyTransform.setOrigin(btVector3(0, 0, z_offset));
-	motionState = new btDefaultMotionState(bodyTransform);
-	btRigidBody* staticBody = new btRigidBody(mass, motionState, shape, localInertia);
-
-	/////////// box with translate spring, attached to static body
-	///////////
-	{
-		mass = m_mass;
-
-		float init_x = 2.0;
-
-		shape = new btBoxShape(btVector3(0.5, 0.5, 0.5));
-		shape->calculateLocalInertia(mass, localInertia);
-		bodyTransform.setIdentity();
-		bodyTransform.setOrigin(btVector3(init_x, 0, z_offset));
-		bodyTransform.getBasis().setEulerZYX(0, 0, 0);
-		motionState = new btDefaultMotionState(bodyTransform);
-        btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, motionState, shape, localInertia);
-        // if deactivation time < some value, then the rigid body will become deactivate and stop
-#if defined(BT_USE_DOUBLE_PRECISION)
-        rbInfo.m_linearSleepingThreshold = 1e-7;
-        rbInfo.m_angularSleepingThreshold = 1e-7;
-#else
-        rbInfo.m_linearSleepingThreshold = 1e-7;
-        rbInfo.m_angularSleepingThreshold = 1e-7;
-#endif
-
-		m_data->m_TranslateSpringBody = new btRigidBody(rbInfo);
-		m_data->m_TranslateSpringBody->setActivationState(ACTIVE_TAG);
-		m_dynamicsWorld->addRigidBody(m_data->m_TranslateSpringBody);
-
-		localA.setIdentity();
-		localB.setIdentity();
-		constraint = new CONSTRAINT_TYPE(*staticBody, *m_data->m_TranslateSpringBody, localA, localB EXTRAPARAMS);
-		constraint->setLimit(0, 5, -5);
-		constraint->setLimit(1, 0, 0);
-		constraint->setLimit(2, 0, 0);
-		constraint->setLimit(3, 0, 0);
-		constraint->setLimit(4, 0, 0);
-		constraint->setLimit(5, 0, 0);
-		constraint->enableSpring(0, true);
-		constraint->setStiffness(0, m_ks);
-		constraint->setDamping(0, m_kd);
-#ifdef USE_6DOF2
-//		constraint->setDamping(0, 0);
-#else
-		constraint->setDamping(5, 1);
-#endif
-		constraint->setEquilibriumPoint(0, 0);
-		constraint->setDbgDrawSize(btScalar(2.f));
-		m_dynamicsWorld->addConstraint(constraint, true);
-	}
-
-    /////////// simulate spring with impulse
-    ///////////
-	{
-		//btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
-		btCollisionShape* shape = new btBoxShape(btVector3(0.5, 0.5, 0.5));
-
-		btScalar mass = m_mass;
-		btVector3 localInertia(0, 0, 0);
-		if (mass > 0)
-			shape->calculateLocalInertia(mass, localInertia);
-
-		btTransform startTransform;
-		startTransform.setIdentity();
-		startTransform.setOrigin(btVector3(2, 0, 0));
-		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
-		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, shape, localInertia);
-		// if deactivation time < some value, then the rigid body will become deactivate and stop
-#if defined(BT_USE_DOUBLE_PRECISION)
-        rbInfo.m_linearSleepingThreshold = 1e-7;
-        rbInfo.m_angularSleepingThreshold = 1e-7;
-#else
-		rbInfo.m_linearSleepingThreshold = 1e-7;
-        rbInfo.m_angularSleepingThreshold = 1e-7;
-#endif
-		m_body = new btRigidBody(rbInfo);
-		m_dynamicsWorld->addRigidBody(m_body);
-	}
-
-	m_guiHelper->autogenerateGraphicsObjects(m_dynamicsWorld);
-}
-
-void Dof6Spring2Setup::stepSimulation(float deltaTime)
-{
-    if ( ((ceil(m_time) - m_time) < 1e-3) || ((m_time - floor(m_time)) < 1e-3) )
-    {
-        btTransform trans;
-        trans = m_body->getWorldTransform();
-        printf("step: %d, time: %f\n", m_step, m_time);
-        printf("pos : %f,%f,%f\n", float(trans.getOrigin().getX()),
-               float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
-        const btVector3 &linear_vel = m_body->getLinearVelocity();
-        printf("vel: %f,%f,%f\n", float(linear_vel.getX()),
-               float(linear_vel.getY()), float(linear_vel.getZ()));
-    }
-
-    // get current pose
-    const btVector3 currPos = m_body->getWorldTransform().getOrigin();
-    const btVector3 currVel = m_body->getLinearVelocity();
-
-    btVector3 impulse = getSpringImpulse(m_ks, m_kd, m_mass, currPos, m_balancePos, currVel, deltaTime);
-    m_body->applyCentralImpulse(impulse);
-
-	m_dynamicsWorld->stepSimulation(deltaTime, 2, btScalar(1.0) / 48);
-
-    m_time += deltaTime;
-    m_step += 1;
-}
+// TODO adjust kd
 
 
 /////////////////////////////////////////////////////////////
@@ -411,15 +157,12 @@ struct Skeleton : public CommonMultiBodyBase
 
     int m_solverType;
     btScalar m_time;
-    int m_nextSec;
     int m_step;
     int m_numLinks;
     btVector3 m_prevBasePos;
     btVector3 m_prevBaseVel;
     btAlignedObjectArray<btQuaternion> m_balanceRot;
     btAlignedObjectArray<btScalar> m_Ks;
-//    btAlignedObjectArray<btScalar> m_jointDamp;
-    btAlignedObjectArray<btQuaternion> m_prevJointRot;
     gravityGenerator m_g;
 
 public:
@@ -444,7 +187,6 @@ Skeleton::Skeleton(struct GUIHelperInterface* helper)
         : CommonMultiBodyBase(helper)
 {
     m_time = btScalar(0.0);
-    m_nextSec = floor(m_time) + 1;
     m_step = 0;
     m_numLinks = 4;
     m_solverType = 0;
@@ -548,7 +290,6 @@ void Skeleton::initPhysics()
     if (m_numLinks > 0)
     {
         btScalar angle = -23.57817848 * SIMD_PI / 180.f * 2;
-//        btScalar angle = 0.0;
         btQuaternion quat0(btVector3(1, 0, 0).normalized(), angle);
         quat0.normalize();
         pMultiBody->setJointPosMultiDof(0, quat0);
@@ -557,12 +298,9 @@ void Skeleton::initPhysics()
     // init default params
     m_Ks.resize(pMultiBody->getNumLinks());
     m_balanceRot.resize(pMultiBody->getNumLinks());
-//    m_jointDamp.resize(pMultiBody->getNumLinks());
-    m_prevJointRot.resize(pMultiBody->getNumLinks());
     for (int i = 0; i < m_numLinks; i++)
     {
         m_balanceRot[i] = btQuaternion(0.0, 0.0, 0.0, 1.0);
-//        m_jointDamp[i] = 0.7;
         m_Ks[i] = 0.3;
     }
 
@@ -754,35 +492,6 @@ void Skeleton::applyTorque(float deltaTime){
             force += spring;
         }
 
-        // damp force
-//        if (m_jointDamp[i] > 0.0) {
-//            btScalar _kd = m_jointDamp[i];
-////             TODO ?
-////            btScalar _kd = adjustKd(m_jointDamp[i], m_multiBody->getLink(i).m_mass, deltaTime);
-////             TODO set rigid body deactive if the speed is very small for a some frames
-//            btVector3 damp_force(m_prevJointRot[i][0] - curr_q[0],m_prevJointRot[i][1] - curr_q[1], m_prevJointRot[i][2] - curr_q[2]);
-//            if (!damp_force.fuzzyZero()) {
-//                damp_force = damp_force.normalized();
-//                btQuaternion vel = m_prevJointRot[i] * curr_q.inverse();
-//                btScalar theta = vel.getAngle() / deltaTime;
-//                // TODO here we only consider the omega, mass and length need to be considered
-//                damp_force *= theta;
-//                // TODO if the omega is big, we need large damp, consider adding the omega * omega
-//                if (damp_force.length() > 0.2)
-//                {
-//                    printf("damp force %f is too big\n", damp_force.length());
-//                    damp_force = damp_force.normalized() * 0.2;
-//                }
-//                if (fabs(damp_force[0]) < 1e-5 && fabs(damp_force[1]) < 1e-5 && fabs(damp_force[2]) < 1e-5) {
-//                    //printf("damp too small: %f, %f, %f\n", damp_force[0], damp_force[1], damp_force[2]);
-//                    if (_kd > 1)
-//                        _kd = 0.9;
-//                }
-//                damp_force *= _kd;
-//            }
-//            force += damp_force;
-//        }
-
         // apply the torque
         for (int d = 0; d < m_multiBody->getLink(i).m_dofCount; d++) {
             m_multiBody->addJointTorqueMultiDof(i, d, force[d]);
@@ -858,18 +567,6 @@ void Skeleton::stepSimulation(float deltaTime) {
     float g = m_g.getGVal();
     m_dynamicsWorld->setGravity(btVector3(0, g, 0));
 
-    if (m_step == 0) {
-        for (int i = 0; i < m_multiBody->getNumLinks(); ++i) {
-            btScalar* q = m_multiBody->getJointPosMultiDof(i);
-            m_prevJointRot[i] = btQuaternion(q[0], q[1], q[2], q[3]);
-        }
-    }
-
-//    if (fabs(m_time - m_nextSec) < deltaTime) {
-//        printf("step: %d, time: %d sec\n", m_step, m_nextSec - 1);
-//        m_nextSec += 1;
-//    }
-
     // calculate and apply the impulse, the damp use the difference between prev pos and curr pos
     applyTorque(deltaTime);
 
@@ -910,12 +607,6 @@ void Skeleton::stepSimulation(float deltaTime) {
     m_prevBasePos = currBasePos;
     m_prevBaseVel = currBaseVel;
 
-    // update prev pos after using it to calculate the damping force before stepSimulation change the pos
-    for (int i = 0; i < m_multiBody->getNumLinks(); ++i) {
-        btScalar* q = m_multiBody->getJointPosMultiDof(i);
-        m_prevJointRot[i] = btQuaternion(q[0], q[1], q[2], q[3]);
-    }
-
     {
         // use cos function to drive m_collider
         btTransform tr;
@@ -929,7 +620,6 @@ void Skeleton::stepSimulation(float deltaTime) {
     }
 
     // capture the frames
-//    if (m_step > 500 && m_step < 550)
 //    {
 //        const char* gPngFileName = "multibody";
 //        sprintf(mfileName, "%s_%d.png", gPngFileName, m_step);
@@ -937,8 +627,8 @@ void Skeleton::stepSimulation(float deltaTime) {
 //    }
 
     // step and update positions
-//    m_dynamicsWorld->stepSimulation(deltaTime);
-    m_dynamicsWorld->stepSimulation(deltaTime, 2, btScalar(1.0) / 48);
+    btScalar  fixedTimeStep = deltaTime / btScalar(2.0);
+    m_dynamicsWorld->stepSimulation(deltaTime, 2, fixedTimeStep);
     if ( WIRE_FRAME ) {
         m_dynamicsWorld->debugDrawWorld();
     }
