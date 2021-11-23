f729fbda95108cf26ec5c3b8963a2391b0cf2823
jingyuc <chenjy@g.ucla.edu>
2021-11-23 18:23:41 -0500

    add missing flag RESET_USE_REDUCED_DEFORMABLE_WORLD in pybullet.c

--- examples/pybullet/examples/reduced_deformable_cube.py
+++ examples/pybullet/examples/reduced_deformable_cube.py
@@ -6,7 +6,8 @@ physicsClient = p.connect(p.GUI)
 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
+# p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
+p.resetSimulation(p.RESET_USE_REDUCED_DEFORMABLE_WORLD)
 p.resetDebugVisualizerCamera(4,-40,-30,[0, 0, 0])
 p.setGravity(0, 0, -10)
 
@@ -14,7 +15,8 @@ tex = p.loadTexture("uvmap.png")
 planeId = p.loadURDF("plane.urdf", [0,0,-2])
 
 # p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, "reduced_cube.mp4")
-cube = p.loadURDF("reduced_cube/deform_cube.urdf", [1,1,1])
+cube = p.loadURDF("reduced_cube/reduced_cube.urdf", [1,1,1])
+# cube = p.loadURDF("reduced_cube/deform_cube.urdf", [1,1,1])
 p.changeVisualShape(cube, -1, rgbaColor=[1,1,1,1], textureUniqueId=tex, flags=0)
 p.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25)
 p.setRealTimeSimulation(0)
