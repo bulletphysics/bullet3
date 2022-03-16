a6554807420c24d7e1d20f559891221e5a1fa318
Paul m. p. P <mail.peny@free.fr>
2022-03-16 21:14:26 +0100

    quick fix
    https://github.com/bulletphysics/bullet3/commit/fb3b306af19aac4d7581702a7676c2452fb9658a introduced dependancy on example folder

--- src/BulletSoftBody/btSoftBodyHelpers.h
+++ src/BulletSoftBody/btSoftBodyHelpers.h
@@ -17,7 +17,31 @@ subject to the following restrictions:
 #define BT_SOFT_BODY_HELPERS_H
 
 #include "btSoftBody.h"
-#include "../../examples/CommonInterfaces/CommonFileIOInterface.h"
+// #include "../../examples/CommonInterfaces/CommonFileIOInterface.h"
+struct CommonFileIOInterface
+{
+	int m_fileIOType;
+	const char* m_pathPrefix;
+
+	CommonFileIOInterface(int fileIOType, const char* pathPrefix)
+		:m_fileIOType(fileIOType),
+		m_pathPrefix(pathPrefix)
+	{
+	}
+
+	virtual ~CommonFileIOInterface()
+	{
+	}
+	virtual int fileOpen(const char* fileName, const char* mode)=0;
+	virtual int fileRead(int fileHandle, char* destBuffer, int numBytes)=0;
+	virtual int fileWrite(int fileHandle,const char* sourceBuffer, int numBytes)=0;
+	virtual void fileClose(int fileHandle)=0;
+	virtual bool findResourcePath(const char* fileName,  char* resourcePathOut, int resourcePathMaxNumBytes)=0;
+	virtual char* readLine(int fileHandle, char* destBuffer, int numBytes)=0;
+	virtual int getFileSize(int fileHandle)=0;
+	virtual void enableFileCaching(bool enable) = 0;
+};
+
 #include <fstream>
 #include <string>
 //
