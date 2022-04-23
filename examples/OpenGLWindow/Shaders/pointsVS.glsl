31b9be82a1e62a14d356cf8f4ccb85abb6e1c5dd
Erwin Coumans <erwin.coumans@gmail.com>
2022-04-23 13:56:40 -0700

    revert lines glsl shader, and add points glsl shader for instanced rendering of point cloud.

--- examples/OpenGLWindow/Shaders/pointsVS.glsl
+++ examples/OpenGLWindow/Shaders/pointsVS.glsl
@@ -3,15 +3,13 @@
 uniform mat4 ModelViewMatrix;
 uniform mat4 ProjectionMatrix;
 uniform vec4 colour;
-
 in vec4 position;
-
-
+in vec4 colourIn;
 out vec4 colourV;
 
 void main (void)
 {
-		colourV = colour;
+		colourV = (colour[3] == -1) ? colourIn : colour;
 		gl_Position = ProjectionMatrix * ModelViewMatrix * position;
 		
 }
