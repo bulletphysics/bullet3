0a8afbbd5654217377038ae76186fb710471b49f
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2022-08-10 09:30:19 +0800

    Update DOF6SpringQuatSetup.cpp
    added the "../CommonInterfaces/CommonExampleInterface.h" header. Trying to resolve the AppVeyor issue.

--- examples/Constraints/DOF6SpringQuatSetup.cpp
+++ examples/Constraints/DOF6SpringQuatSetup.cpp
@@ -10,6 +10,8 @@
 #include "BulletDynamics/ConstraintSolver/btGeneric6DofSpring2Constraint.h"
 #include "BulletDynamics/ConstraintSolver/btGeneric6DofSpringConstraintQuaternion.h"
 
+#include "../CommonInterfaces/CommonExampleInterface.h"
+
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
