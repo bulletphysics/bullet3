1ab30d26331fdad62238f76664ed782891e7b407
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2022-09-05 18:17:17 +0200

    Fix registration of gym envs

--- examples/pybullet/gym/pybullet_envs/__init__.py
+++ examples/pybullet/gym/pybullet_envs/__init__.py
@@ -1,13 +1,5 @@
 import gym
-from gym.envs.registration import registry, make, spec
-
-
-def register(id, *args, **kvargs):
-  if id in registry.env_specs:
-    return
-  else:
-    return gym.envs.registration.register(id, *args, **kvargs)
-
+from gym.envs.registration import registry, make, spec, register
 
 # ------------bullet-------------
 
